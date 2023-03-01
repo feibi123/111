b127cd629a2d68ec0072614a4dc81d6799403d5e
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-01 17:31:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
-uploaded_file = st.file_uploader("上传订单报告", type="csv")
-df = pd.read_csv(uploaded_file, header=None, encoding='gbk')  # header=None 参数禁止将第一行读入为列标题
-df = df.drop(df.index[:7]) 
+uploaded_file3 = st.file_uploader("上传产品属性表", type="xlsx")  # 读取产品属性表，并将首行作为标题列
+dc = pd.read_excel(uploaded_file3, header=0)
 st.dataframe(df)
