7614a9facb5cfd37ec1a3bd96b009eef9eb17705
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-01 17:59:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import openpyxl
 import streamlit as st
 import pandas as pd
-uploaded_file2 = st.file_uploader("上传即时库存", type="csv")
+uploaded_file2 = st.file_uploader("上传在途库存", type="csv")
 dk = pd.read_csv(uploaded_file2, header=0, encoding='gbk') 
 st.dataframe(dk)
