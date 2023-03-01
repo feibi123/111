6147c7f53dd9747af721b7ad30e2e39aca959e1c
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-01 17:32:37 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import streamlit as st
 import pandas as pd
 uploaded_file2 = st.file_uploader("上传即时库存", type="csv")
 dk = pd.read_csv(uploaded_file2, header=0, encoding='gbk') 
-st.dataframe(df)
+st.dataframe(dk)
