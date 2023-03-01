651a5ac7d4b21027420cbc18db3158aa957ade0e
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-01 17:26:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
-uploaded_file1 = st.file_uploader("上传在途库存", type=["xlsx", "xls"])  # 读取在途库存，并将首行作为标题列
+uploaded_file1 = st.file_uploader("上传在途库存", type=["xlrd", "xls"])  # 读取在途库存，并将首行作为标题列
 dt = pd.read_excel(uploaded_file1, header=0)
 st.dataframe(df)
