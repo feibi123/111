3f72e5d1d579f8cdb5581c2c765fe46fe995d799
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 10:45:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
-import subprocess
+import streamlit as st
+import chardet
 
-subprocess.check_call(["pip", "install", "chardet"])
+# 上传文件并读取文件内容
+uploaded_file = st.file_uploader("上传文件", type=["csv"])
+if uploaded_file is not None:
+    # 读取文件内容
+    content = uploaded_file.getvalue()
+    
+    # 自动检测文件编码
+    result = chardet.detect(content)
+    file_encoding = result["encoding"]
+    
+    # 使用检测到的编码读取文件
+    if file_encoding is not None:
+        st.write(f"文件编码: {file_encoding}")
+        content = content.decode(file_encoding)
+    else:
+        st.write("未能检测到文件编码")
+    
+    # 将文件内容转换为 Pandas DataFrame
+    df = pd.read_csv(io.StringIO(content), header=0)
+    st.write(df)
