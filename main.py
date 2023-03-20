2dca124bae9cc3113fe3f96b81edb467d50c2c26
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 15:58:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 uploaded_file1 = st.sidebar.file_uploader("上传订单报告", type="csv")
-if uploaded_file1 is not None:
-        df = pd.read_csv(uploaded_file1, header=None, encoding='GB2312')
-    st.table(df)
-else:
-    st.write("请上传CSV文件")
+df = pd.read_csv(uploaded_file1, header=None, encoding='GB2312')
+st.table(df)
+
