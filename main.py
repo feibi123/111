474d71021c38d1188e94e4015060e3a3266a07dd
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 17:25:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ uploaded_file = st.sidebar.file_uploader("上传订单报告", type="csv")
 
 if uploaded_file is not None:
     # 使用 Pandas 读取 CSV 文件
-    df = pd.read_csv(uploaded_file, header=None, sep = None)
+    df = pd.read_csv(uploaded_file, header=None, sep = '\t')
     # 显示数据框
     st.write(df)
