a3f7a92765fea905b7e31dea10aa714f4e77619e
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 12:37:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
-uploaded_file = st.file_uploader("选择文件", type=None)
+uploaded_file = st.file_uploader("上传文件", type=["csv"], accept_multiple_files=False)
 
 if uploaded_file is not None:
-    # 处理上传的文件
-    contents = uploaded_file.read()
-    st.write("上传的文件内容为：", contents)
+    file_contents = uploaded_file.read()
+    st.write("上传的文件内容：")
+    st.write(file_contents)
