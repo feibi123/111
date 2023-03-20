404cbf7ad49bc1fdf9299c32e1ba8ea5ce341db2
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 17:07:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ import streamlit as st
 
 uploaded_file1 = st.sidebar.file_uploader("上传订单报告", type="csv")
 
-df = pd.read_csv(uploaded_file1, header=None)  # header=None 参数禁止将第一行读入为列标题
+df = pd.read_csv(uploaded_file1)  # header=None 参数禁止将第一行读入为列标题
 df = df.drop(df.index[:7])  # 删除前7行
 st.table(df)
