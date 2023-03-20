7e014e58afe8eedb881f6783760b62039d87f003
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 12:36:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 import streamlit as st
-import codecs
 
-def read_uploaded_file(file):
-    with codecs.open(file, 'r', encoding=codecs.lookup(file.name).name) as f:
-        file_contents = f.read()
-    return file_contents
+uploaded_file = st.file_uploader("选择文件", type=None)
 
-def main():
-    st.title("文件上传示例")
-    uploaded_files = st.file_uploader("上传文件", type=["txt", "csv"])
-    if uploaded_files is not None:
-        file_data = []
-        for file in uploaded_files:
-            file_dict = {"name": file.name, "content": file.read()}
-            file_data.append(file_dict)
-
-        for file in file_data:
-            st.write(f"文件名：{file['name']}")
-            st.write(f"文件内容：{read_uploaded_file(file['content'])}")
-
-if __name__ == "__main__":
-    main()
+if uploaded_file is not None:
+    # 处理上传的文件
+    contents = uploaded_file.read()
+    st.write("上传的文件内容为：", contents)
