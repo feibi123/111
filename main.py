736f1b9c127f32353a21228e043f54aab340cca4
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 14:05:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import pandas as pd
-
-uploaded_file = st.file_uploader("Choose an XLSX file", type="xlsx")
-
-if uploaded_file is not None:
-    # 使用 Pandas 库中的 read_excel 函数读取 XLSX 文件
-    df = pd.read_excel(uploaded_file)
-    st.write("File content:")
-    st.write(df)
+uploaded_file = st.sidebar.file_uploader("上传产品属性表", type="csv")
+df = pd.read_csv(uploaded_file)
+st.table(df)
