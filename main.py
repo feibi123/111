6ed4d27cc1393ea09c01a53a3a24d4a0eba73f33
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 15:38:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import pandas as pd
 uploaded_file1 = st.sidebar.file_uploader("上传订单报告", type="csv")
 df = pd.read_csv(uploaded_file1, header=None)  # header=None 参数禁止将第一行读入为列标题
 df = df.drop(df.index[:7])  # 删除前7行
+df.columns = df.iloc[0]  # 将第八行作为标题
+df = df.drop(df.index[0]) 
 st.table(df)
