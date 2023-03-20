154675ac6fd86ab710e7793b431daf78aa77a4b6
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 14:55:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 uploaded_file = st.sidebar.file_uploader("上传产品属性表", type="xlsx")
-df = pd.read_excel(uploaded_file, header=0,
+df = pd.read_excel(uploaded_file, header=0)
 st.table(df)
