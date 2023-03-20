8f8fa88d5c57d9d539e0e8a2c40293fbcfa90839
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 12:47:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import pandas as pd
-from io import StringIO
 
-uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+uploaded_file = st.file_uploader("Choose an XLSX file", type="xlsx")
 
 if uploaded_file is not None:
-    content = uploaded_file.read().decode("utf-8")
-    df = pd.read_csv(StringIO(content))
+    # 使用 Pandas 库中的 read_excel 函数读取 XLSX 文件
+    df = pd.read_excel(uploaded_file)
     st.write("File content:")
     st.write(df)
