ec8f9a25eac97cf1fd78077d32812e97e1859f10
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 17:49:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import codecs
+from io import StringIO
 
 # 设置页面标题
 st.title("上传 CSV 文件示例")
@@ -14,12 +15,15 @@ if uploaded_file is not None:
     content = uploaded_file.read()
     decoded_content = codecs.decode(content, 'utf-8-sig') # 解码为 utf-8
 
+    # 将文件内容转换为字符串
+    csv_string = StringIO(decoded_content)
+
     # 尝试使用 Pandas 读取 utf-8 编码的文件
     try:
-        df = pd.read_csv(StringIO(decoded_content), skiprows=7)
+        df = pd.read_csv(csv_string, , skiprows=7)
     except:
         # 如果读取失败，尝试使用 Pandas 读取 gbk 编码的文件
-        df = pd.read_csv(StringIO(decoded_content), skiprows=7, encoding='gbk')
+        df = pd.read_csv(csv_string, skiprows=7, encoding='gbk')
 
     # 显示数据框
     st.write(df)
