cd7045969e876a69309c11af8168c63e7ae3eff4
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 11:11:28 +0800

    Update streamlit数据可视化.py

--- streamlit数据可视化.py
+++ streamlit数据可视化.py
@@ -13,7 +13,7 @@ uploaded_file1 = st.sidebar.file_uploader("上传订单报告", type="csv")
 uploaded_file2 = st.sidebar.file_uploader("上传库存表", type="csv")
 uploaded_file = st.sidebar.file_uploader("上传产品属性表", type="csv")
 
-df = pd.read_csv(uploaded_file1, header=None, encoding='GB2312')  # header=None 参数禁止将第一行读入为列标题
+df = pd.read_csv(uploaded_file1, header=None, encoding='gbk')  # header=None 参数禁止将第一行读入为列标题
 df = df.drop(df.index[:7])  # 删除前7行
 df.columns = df.iloc[0]  # 将第八行作为标题
 df = df.drop(df.index[0])  # 删除第八行
