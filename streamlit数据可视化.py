54f50c463d114f80bc21d22413669e354603a40e
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 10:13:11 +0800

    Update streamlit数据可视化.py

--- streamlit数据可视化.py
+++ streamlit数据可视化.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import codecs
 pd.set_option('display.max_colwidth', None)
 st.set_page_config(layout="wide")
 
@@ -12,6 +13,10 @@ variable4 = col1.number_input("输入最小安全库存", min_value=1, max_value
 uploaded_file1 = st.sidebar.file_uploader("上传订单报告", type="csv")
 uploaded_file2 = st.sidebar.file_uploader("上传库存表", type="csv")
 uploaded_file = st.sidebar.file_uploader("上传产品属性表", type="csv")
+raw_data = uploaded_file.read()
+file_encoding = codecs.lookup(raw_data).name
+dc = pd.read_csv(uploaded_file, header=0, encoding=file_encoding)
+dc = dc[['产品类别', '颜色', 'sku']]  # 只保留链接名称、父ASIN和sku列
 
 df = pd.read_csv(uploaded_file1, header=None, encoding='GB2312')  # header=None 参数禁止将第一行读入为列标题
 df = df.drop(df.index[:7])  # 删除前7行
@@ -55,8 +60,8 @@ dt = dt.reindex(columns=cols1)
 
 df = pd.merge(df, dt, on='sku', how='outer')  # 将7天销量表格、15天销量表格、可变销量表格和在库在途库存表格合并
 
-dc = pd.read_csv(uploaded_file, header=0, encoding=uploaded_file.encoding)
-dc = dc[['产品类别', '颜色', 'sku']]  # 只保留链接名称、父ASIN和sku列
+# dc = pd.read_csv(uploaded_file, header=0, encoding=uploaded_file.encoding)
+# dc = dc[['产品类别', '颜色', 'sku']]  # 只保留链接名称、父ASIN和sku列
 
 df = pd.merge(df, dc, on='sku', how='left')  # 将7天销量表格、15天销量表格、可变销量表格、在途库存表格、在库库存表格和产品属性表合并
 df.fillna(0, inplace=True)  # 将所有空值替换为0
