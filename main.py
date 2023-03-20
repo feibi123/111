7cf3f7317e5b1816861e01a10cb3cd0996ae2c2d
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 15:41:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 uploaded_file1 = st.sidebar.file_uploader("上传订单报告", type="csv")
-df = pd.read_csv(uploaded_file1, header=None)  # header=None 参数禁止将第一行读入为列标题
-# df = df.drop(df.index[:7])  # 删除前7行
-# df.columns = df.iloc[0]  # 将第八行作为标题
-# df = df.drop(df.index[0]) 
-st.table(df)
+if uploaded_file1 is not None:
+    df = pd.read_csv(uploaded_file1, header=None)
+    st.table(df)
+else:
+    st.write("请上传CSV文件")
