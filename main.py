22a241e561048e441205cae26ebfa758c0fe737a
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-20 15:58:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-import streamlit as st
+import io
 import pandas as pd
-uploaded_file1 = st.sidebar.file_uploader("上传订单报告", type="csv")
-df = pd.read_csv(uploaded_file1, header=None, encoding='GB2312')
-st.table(df)
+import streamlit as st
 
+uploaded_file1 = st.sidebar.file_uploader("上传订单报告", type="csv")
+
+if uploaded_file1 is not None:
+    # 将文件内容读取到内存中
+    file_contents = uploaded_file1.read()
+    
+    # 将字节数据转换为文本数据
+    file_text = io.TextIOWrapper(io.BytesIO(file_contents), encoding='gbk').read()
+    
+    # 使用pandas读取csv文件
+    df = pd.read_csv(io.StringIO(file_text), header=None)
+    
+    # 显示数据
+    st.write(df)
