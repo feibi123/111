51843e272f9c04c55869d4deb03787ca4344e60a
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-21 10:59:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,50 @@
-import streamlit as st
 import pandas as pd
-from io import StringIO
-from streamlit_aggrid import st_aggrid
+import streamlit as st
 
-# pd.set_option('display.max_colwidth', None)
-st.set_page_config(layout="wide")
-# 添加上传文件的按钮
-uploaded_file = st.file_uploader("Choose a file")
-
-# 如果用户上传了文件
-if uploaded_file is not None:
-    # 读取文件内容
-    content = uploaded_file.read()
-
-    # 尝试使用 utf-8 编码方式进行解码
+def read_csv_file(uploaded_file):
+    # 读取上传的 CSV 文件
     try:
-        decoded_content = content.decode('utf-8')
-    except UnicodeDecodeError:
-        # 如果解码失败，则尝试使用 gbk 编码方式进行解码
-        decoded_content = content.decode('gbk')
-
-    # 将解码后的文件内容转换为 pandas 数据框
-    df = pd.read_csv(StringIO(decoded_content), skiprows=7)  
-    df = df.dropna(subset=['quantity'])  # 删除含有空值的行
-    df['quantity'] = df['quantity'].astype(int)  # 将quantity列转换成整数类型
-    df = df.dropna(subset=['type'])   # 删除含有空值的行
-    df = df[df['type'].str.contains('Order')]  # 从type列筛选出Order
-    df = df[['date/time', 'sku', 'quantity']]
-    st_aggrid(df, height=500, gridOptions={
-              'rowDragManaged': True, 'rowHeight': 40})
-    # 冻结表头
-    st.write("<style>.ag-header { position: sticky; top: 0; z-index: 999; }</style>", unsafe_allow_html=True)
-
-    # 冻结上传文件按钮
-    st.write("<style>.stFileUploader { position: sticky; top: 0; z-index: 999; }</style>", unsafe_allow_html=True)
+        df = pd.read_csv(uploaded_file)
+    except Exception as e:
+        st.error(f"读取文件失败：{e}")
+        return None
     
-    st.write(df)
+    return df
+
+def freeze_table_header(df):
+    # 将数据框的第一行作为表头，并将其从数据框中删除
+    header = df.iloc[0]
+    df = df[1:]
+
+    # 使用 set_index() 方法将表头转换为行索引
+    df = df.set_index(header)
+
+    # 显示上传文件的按钮
+    uploaded_file = st.file_uploader("上传文件")
+
+    # 在显示的表格上方添加一个新的表格，其中只有一行，该行的值与原始数据框的表头相同
+    new_table = st.empty()
+    new_table.add_rows(1)
+    new_table[0, :].data = header
+
+    # 显示数据框，此时表头已经成为了行索引
+    table = st.table(df)
+
+    # 将上传文件的按钮、新表格和原始表格合并
+    merged_table = f"{uploaded_file} {new_table._get_value()}\n{table._get_value()}"
+    table._update_html(merged_table)
+
+# 主程序
+st.set_page_config(page_title="冻结表头和上传文件", layout="wide")
+
+# 显示页面标题和说明文本
+st.title("冻结表头和上传文件")
+st.write("这是一个示例页面，用于演示如何冻结表头并上传文件。")
+
+# 显示上传文件的按钮，并读取文件内容
+uploaded_file = st.file_uploader("选择要上传的文件", type="csv")
+if uploaded_file is not None:
+    df = read_csv_file(uploaded_file)
+    if df is not None:
+        # 冻结表头并显示数据框
+        freeze_table_header(df)
