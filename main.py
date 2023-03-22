a87352afb0240ea1a93287769d144f3336a2da8f
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-22 15:27:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,23 @@ st.write("""
     .freeze {
         position: sticky;
         top: 0;
+        z-index: 1;
         background-color: white;
+        font-weight: bold;
+    }
+    /* 设置表格边框和宽度 */
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+    th, td {
+        border: 1px solid black;
+        padding: 8px;
+        text-align: left;
+        font-size: 14px;
     }
 </style>
 """, unsafe_allow_html=True)
 
-st.table(df.head(20).style.set_table_attributes("class='freeze'"))
+# 将表格呈现为HTML表格，并对首行应用CSS类
+st.write(df.head(20).to_html(index=False, classes=["freeze", "table"], escape=False), unsafe_allow_html=True)
