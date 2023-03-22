0fa56a81db5bb0d812f25390ff08170599c9463f
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-22 15:22:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,21 +2,8 @@ import streamlit as st
 import pandas as pd
 from io import StringIO
 uploaded_file1 = st.sidebar.file_uploader("上传订单报告")
-# 如果用户上传了文件
-if uploaded_file1 is not None:
-    # 读取文件内容
-    content = uploaded_file1.read()
-
-    # 尝试使用 utf-8 编码方式进行解码
-    try:
-        decoded_content = content.decode('utf-8')
-    except UnicodeDecodeError:
-        # 如果解码失败，则尝试使用 gbk 编码方式进行解码
-        decoded_content = content.decode('gbk')
  # 将解码后的文件内容转换为 pandas 数据框
-    df = pd.read_csv(StringIO(decoded_content), skiprows=7)
-    
- 
+df = pd.read_csv(uploaded_file1, skiprows=7)
 df = df.dropna(subset=['quantity'])
 st.write("""
 <style>
