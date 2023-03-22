587b206a362fbabee89659f7f0e0b7c372182149
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-22 14:08:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,19 @@
 import streamlit as st
-import pandas as pd
 
-df = pd.DataFrame({
-    "产品类别": ["A", "A", "B", "C"],
-    "是否发货": ["是", "否", "是", "否"],
-    "销售数量": [20, 50, 180, 10]
-})
+st.write("# My Streamlit App")
 
-st.set_page_config(layout="wide")
-selected_links = st.multiselect("选择产品", df["产品类别"].unique())
-selected_links1 = st.multiselect("是否发货", df["是否发货"].unique())
+# 在页面中固定一个块
+st.write("## Fixed Block")
 
-# 创建容器
-container = st.beta_container()
+# 在页面中固定两个复选框
+st.write('<div style="position: fixed; top: 50px; left: 50px;">')
+selected_links = st.multiselect("选择产品", ["产品1", "产品2", "产品3"])
+selected_links1 = st.multiselect("是否发货", ["是", "否"])
+st.write('</div>')
 
-# 在容器中放置复选框
-with container:
-    # 使用 CSS 样式表固定复选框的位置
-    st.markdown("""
-    <style>
-    .st-ct {
-        position: fixed !important;
-        top: 100px;
-        left: 50px;
-    }
-    </style>
-    """, unsafe_allow_html=True)
+# 显示一些示例内容
+st.write("## Example Content")
+st.write("This is some example content.")
+st.write("This is more example content.")
+st.write("This is even more example content.")
 
-    st.write(selected_links)
-    st.write(selected_links1)
-
-# 显示数据框
-df = df[df["产品类别"].isin(selected_links) & df["是否发货"].isin(selected_links1)]
-st.table(df)
