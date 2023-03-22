04e6e7fca3256558a948bdfaad8bd0f3bc7d71ff
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-22 15:18:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 import streamlit as st
+import pandas as pd
+uploaded_file1 = st.sidebar.file_uploader("上传订单报告")
+# 如果用户上传了文件
+if uploaded_file1 is not None:
+    # 读取文件内容
+    content = uploaded_file1.read()
 
-data = [
-    ['A1', 'B1', 'C1', 'D1', 'E1'],
-    ['A2', 'B2', 'C2', 'D2', 'E2'],
-    ['A3', 'B3', 'C3', 'D3', 'E3'],
-    ['A4', 'B4', 'C4', 'D4', 'E4'],
-    ['A5', 'B5', 'C5', 'D5', 'E5'],
-    ['A6', 'B6', 'C6', 'D6', 'E6'],
-    ['A7', 'B7', 'C7', 'D7', 'E7'],
-    ['A8', 'B8', 'C8', 'D8', 'E8'],
-    ['A9', 'B9', 'C9', 'D9', 'E9'],
-    ['A10', 'B10', 'C10', 'D10', 'E10']
-]
+    # 尝试使用 utf-8 编码方式进行解码
+    try:
+        decoded_content = content.decode('utf-8')
+    except UnicodeDecodeError:
+        # 如果解码失败，则尝试使用 gbk 编码方式进行解码
+        decoded_content = content.decode('gbk')
 
-st.write('Frozen Columns with Scrollable Body')
-left, right = st.beta_columns([1, 3])
-with left:
-    st.write('Column 1 (Frozen)')
-with right:
-    st.write('Columns 2-5 (Scrollable)')
-    for row in data:
-        col1, col2, col3, col4, col5 = st.beta_columns([1, 1, 1, 1, 1])
-        col1.write(row[0])
-        col2.write(row[1])
-        col3.write(row[2])
-        col4.write(row[3])
-        col5.write(row[4])
+    # 将解码后的文件内容转换为 pandas 数据框
+    df = pd.read_csv(StringIO(decoded_content), skiprows=7)
+    
+st.write("""
+<style>
+    /* 冻结表格首行 */
+    .freeze {
+        position: sticky;
+        top: 0;
+        background-color: white;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+st.table(df.head(20).style.set_table_attributes("class='freeze'"))
