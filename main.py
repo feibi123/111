6bd6b2ca00fe73b02abb8f78de078f532d449713
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-22 11:14:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import pandas as pd
 import streamlit as st
 from io import StringIO
-from streamlit_pandas_profiling import st_profile_report
-st.set_page_config(layout="wide")
+from streamlit.elements import layout_utils
+from streamlit.proto import BlockPath_pb2
+
+def set_block_container_style(display="flex"):
+    """Set CSS style attributes to make elements flex and sticky"""
+    st.markdown(
+        f"""
+        <style>
+            #root {{
+                display: {display};
+            }}
+            .stBlockContainer {{
+                flex: 1;
+            }}
+            .streamlit-sticky.sticky-header {{
+                top: 0px;
+                z-index: 10000;
+                position: sticky !important;
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+    
+ set_block_container_style()
 # 读取上传的文件
 uploaded_file = st.file_uploader("Choose a file")
 
@@ -21,4 +44,11 @@ if uploaded_file is not None:
     # 将解码后的文件内容转换为 pandas 数据框
     df = pd.read_csv(StringIO(decoded_content), skiprows=7)
     
-    st.table(df.style.set_table_styles([{'selector': 'thead', 'sticky': True}]))
+    set_block_container_style(display="block")
+    block = layout_utils.empty()
+    block_path = BlockPath_pb2.BlockPath(path=[block.id])
+    block.stickyHeaders = [block_path]
+    block.stickyFooters = [block_path]
+
+    # Display the data frame
+    st.dataframe(df)
