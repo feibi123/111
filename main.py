b214f4fdb93d386974ee6db90adf469173990644
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-22 15:10:27 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import streamlit as st
 
-st.write("# My Streamlit App")
+data = [
+    ['A1', 'B1', 'C1', 'D1', 'E1'],
+    ['A2', 'B2', 'C2', 'D2', 'E2'],
+    ['A3', 'B3', 'C3', 'D3', 'E3'],
+    ['A4', 'B4', 'C4', 'D4', 'E4'],
+    ['A5', 'B5', 'C5', 'D5', 'E5'],
+    ['A6', 'B6', 'C6', 'D6', 'E6'],
+    ['A7', 'B7', 'C7', 'D7', 'E7'],
+    ['A8', 'B8', 'C8', 'D8', 'E8'],
+    ['A9', 'B9', 'C9', 'D9', 'E9'],
+    ['A10', 'B10', 'C10', 'D10', 'E10']
+]
 
-# 在页面中固定一个块
-st.write("## Fixed Block")
-
-# 在页面中固定两个复选框
-st.write('<div style="position: fixed; top: 50px; left: 50px;">')
-selected_links = st.multiselect("选择产品", ["产品1", "产品2", "产品3"])
-selected_links1 = st.multiselect("是否发货", ["是", "否"])
-st.write('</div>')
-
-# 显示一些示例内容
-st.write("## Example Content")
-st.write("This is some example content.")
-st.write("This is more example content.")
-st.write("This is even more example content.")
-
+st.write('Frozen Columns with Scrollable Body')
+left, right = st.beta_columns([1, 3])
+with left:
+    st.write('Column 1 (Frozen)')
+with right:
+    st.write('Columns 2-5 (Scrollable)')
+    for row in data:
+        col1, col2, col3, col4, col5 = st.beta_columns([1, 1, 1, 1, 1])
+        col1.write(row[0])
+        col2.write(row[1])
+        col3.write(row[2])
+        col4.write(row[3])
+        col5.write(row[4])
