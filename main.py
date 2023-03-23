6d53d0422fd9aebf2c8a41e4ace6e9a26fab8786
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 17:14:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,42 +7,4 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '性别': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 定义CSS样式
-css = """
-<style>
-table.dataframe {
-    width: 100%;
-    border-collapse: collapse;
-}
-
-table.dataframe th {
-    position: sticky;
-    top: 0;
-    background: white;
-    border: 1px solid #d3d3d3;
-    text-align: center;
-    padding: 5px 0;
-}
-
-table.dataframe th:first-child {
-    left: 0;
-    z-index: 1;
-}
-
-table.dataframe td {
-    border: 1px solid #d3d3d3;
-    text-align: center;
-    padding: 5px 0;
-}
-
-div.stTableContainer {
-    height: 600px;
-    overflow-y: scroll;
-}
-
-</style>
-"""
-
-# 显示数据
-st.markdown(css, unsafe_allow_html=True)
-st.table(df)
+st.dataframe(df)
