d5e930b71125bf74358b55f2c34cb0d280389a7f
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 11:18:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@
 import streamlit as st
 import pandas as pd
 
+pd.set_option('display.max_colwidth', None)
+st.set_page_config(layout="wide")
+
 # 创建示例数据
 data = {'A': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25],
         'B': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25],
