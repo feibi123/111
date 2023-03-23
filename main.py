deb9d8986396bfec0af7040d4dfe62b3d1769900
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 14:13:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,19 +7,29 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '性别': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 将第一行固定
+# 将表格铺满全屏
 st.markdown(
     """
     <style>
-        /* 首行固定 */
-        .scrollable-table > table > thead > tr {
+        .scrollable-table {
             position: fixed;
             top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            z-index: 9999;
+            overflow: auto;
         }
 
-        /* 第二行作为滚动条的起点 */
-        .scrollable-table > table > tbody > tr:nth-child(2) {
-            margin-top: 40px;
+        .scrollable-table table {
+            width: 100%;
+        }
+
+        /* 固定表头 */
+        .scrollable-table thead th {
+            position: sticky;
+            top: 0;
+            background-color: #ffffff;
         }
     </style>
     """,
