392a1f27edfe757337ba420eb2e879a6bafd7acd
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 14:27:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,33 +2,25 @@ import streamlit as st
 import pandas as pd
 
 # 创建示例数据
-data = {'Name': ['小明', '小红', '小刚'] * 100,
-        'Age': [18, 19, 20] * 100,
-        'Gender': ['男', '女', '男'] * 100}
+data = {'姓名': ['小明', '小红', '小刚'] * 100,
+        '年龄': [18, 19, 20] * 100,
+        '性别': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
 # 设置表格样式
 st.markdown(
     """
     <style>
-        #my_table td:first-child {
-            position: -webkit-sticky;
-            position: sticky;
-            left: 0;
-            z-index: 1;
-            background-color: white;
+        .scrollable-table-container {
+            position: relative;
+            overflow: auto;
+            height: 80vh;
         }
-        #my_table thead th {
-            background-color: white;
-        }
-        .fullscreen {
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
+
+        .scrollable-table-container table {
             width: 100%;
-            height: 100%;
+            position: sticky;
+            top: 0;
         }
     </style>
     """,
@@ -37,4 +29,4 @@ st.markdown(
 
 # 显示表格
 with st.beta_container():
-    st.write("<div class='fullscreen'><table id='my_table' class='scrollable-table'>", df.to_html(index=False), "</table></div>", unsafe_allow_html=True)
+    st.write("<div class='scrollable-table-container'>", df.to_html(index=False), "</div>", unsafe_allow_html=True)
