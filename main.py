591a2fb6e78bec87eef27d5da03d75b8cef2d9cb
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 14:30:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,40 +2,56 @@ import streamlit as st
 import pandas as pd
 
 # 创建示例数据
-data = {'姓名': ['小明', '小红', '小刚'] * 100,
-        '年龄': [18, 19, 20] * 100,
-        '性别': ['男', '女', '男'] * 100}
+data = {'Name': ['John', 'Emily', 'Michael'] * 100,
+        'Age': [25, 30, 35] * 100,
+        'Gender': ['M', 'F', 'M'] * 100}
 df = pd.DataFrame(data)
-
-# 设置表格样式
-st.markdown(
-    """
-    <style>
-        .freeze-row::-webkit-scrollbar {
-            display: none;
-        }
-        .freeze-row {
-            position: sticky;
-            top: 0;
-            z-index: 1;
-        }
-        .freeze-row th {
-            background-color: white !important;
-            position: -webkit-sticky;
-            position: sticky;
-            top: 0;
-        }
-        .freeze-row td {
-            background-color: white !important;
-        }
-        .full-width {
-            width: 100%;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
 
 # 显示表格
 with st.beta_container():
-    st.table(df.style.set_table_attributes('class="freeze-row full-width"').hide_index())
+    # 设置表格样式
+    st.markdown(
+        """
+        <style>
+            .scrollable-table {
+                position: fixed;
+                top: 80px;
+                left: 0;
+                width: 100%;
+                height: calc(100% - 80px);
+                z-index: 9999;
+                overflow: auto;
+            }
+
+            .scrollable-table table {
+                width: 100%;
+            }
+            
+            .scrollable-table thead th {
+                position: sticky;
+                top: 0;
+                background-color: white;
+            }
+            
+            .scrollable-table thead th:first-child {
+                left: 0;
+                z-index: 1;
+            }
+            
+            .scrollable-table thead th:nth-child(2) {
+                left: 120px;
+            }
+            
+            .scrollable-table thead th:nth-child(3) {
+                left: 200px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    st.write("<div class='scrollable-table'>", df.iloc[1:].to_html(index=False), "</div>", unsafe_allow_html=True)
+
+    # 显示表头
+    st.write("<div class='scrollable-table'>", df.iloc[:1].to_html(index=False), "</div>", unsafe_allow_html=True)
+
