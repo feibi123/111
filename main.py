12a07f69312ff85f64ea11a2def53e4058a4ead7
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 17:38:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,11 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '性别': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 冻结表格的首行
-def freeze_table(df):
-    # 获取表格HTML代码
-    table_html = df.to_html(index=False, classes=['freeze'])
-    # 在表格HTML代码中添加CSS样式
-    styled_html = '<style>.freeze tbody tr:first-child {position: sticky; top: 0; background-color: white;}</style>' + table_html
-    return styled_html
+# 将DataFrame转换为Styler对象，并添加CSS样式
+styled_df = df.style.set_table_styles([{
+    'selector': 'thead th',
+    'props': [('position', 'sticky'), ('top', '0'), ('background-color', 'white')]
+}])
 
-# 显示冻结首行的表格
-st.write(freeze_table(df), unsafe_allow_html=True)
+# 在Streamlit中显示表格
+st.table(styled_df)
