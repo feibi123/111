c4e7903754d35684017a4630d5160cd8a47eb53a
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 17:46:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,18 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '性别': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 设置首行冻结样式
-styles = [dict(selector="th", props=[("position", "sticky"), ("top", "0"), ("background-color", "white")])]
+# 添加CSS样式
+st.markdown("""
+            <style>
+            table td:nth-child(1) {
+                position: -webkit-sticky;
+                position: sticky;
+                left: 0;
+                background-color: white;
+                z-index: 1;
+            }
+            </style>
+            """, unsafe_allow_html=True)
 
 # 显示表格
-st.table(df.style.set_table_styles(styles).set_properties(**{'position': 'sticky', 'top': '0'}))
+st.table(df)
