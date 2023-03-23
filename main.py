61180bc0d1b5fe5c46ef9d431d5f0e748e04e85f
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 17:48:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,18 +7,37 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '性别': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 添加CSS样式
-st.markdown("""
-            <style>
-            table td:nth-child(1) {
-                position: -webkit-sticky;
-                position: sticky;
-                left: 0;
-                background-color: white;
-                z-index: 1;
-            }
-            </style>
-            """, unsafe_allow_html=True)
+# 设置表格样式
+st.markdown(
+    """
+    <style>
+        /* 设置表格样式 */
+        .scrollable-table {
+            height: 600px;
+            overflow-y: scroll;
+            position: sticky;
+            top: 0;
+            background-color: white;
+            z-index: 1;
+        }
+
+        /* 设置表头样式 */
+        .scrollable-table th {
+            position: sticky;
+            top: 0;
+            background-color: white;
+            z-index: 2;
+        }
+
+        /* 设置表格行的样式 */
+        .scrollable-table tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # 显示表格
-st.table(df)
+with st.beta_container():
+    st.table("<div class='scrollable-table'>", df.to_html(index=False), "</div>", unsafe_allow_html=True)
