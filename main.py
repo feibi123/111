a12457886e0eeaaf863b9cfaf633da22a288f72e
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 13:38:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,36 +2,19 @@ import streamlit as st
 import pandas as pd
 
 # 创建示例数据
-data = {'姓名': ['小明', '小红', '小刚'] * 100,
-        '年龄': [18, 19, 20] * 100,
-        '性别': ['男', '女', '男'] * 100}
+data = {'Name': ['Alice', 'Bob', 'Charlie'] * 100,
+        'Age': [18, 19, 20] * 100,
+        'Gender': ['Female', 'Male', 'Male'] * 100}
 df = pd.DataFrame(data)
-
-# 重命名列名
-df = df.rename(columns={'姓名': 'Name', '年龄': 'Age', '性别': 'Gender'})
 
 # 设置表格样式
 st.markdown(
     """
     <style>
-        .scrollable-table {
-            position: fixed;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            z-index: 9999;
-            overflow: auto;
-        }
-
-        .scrollable-table table {
-            width: 100%;
-        }
-        
-        .scrollable-table thead {
+        #mytable thead {
             position: sticky;
             top: 0;
-            background-color: #fff;
+            background-color: black;
         }
     </style>
     """,
@@ -40,5 +23,4 @@ st.markdown(
 
 # 显示表格
 with st.beta_container():
-    st.write("<div class='scrollable-table'><table><thead><tr><th>Name</th><th>Age</th><th>Gender</th></tr></thead>",
-             df.to_html(index=False), "</table></div>", unsafe_allow_html=True)
+    st.table(df)
