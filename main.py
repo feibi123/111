657cc750c871b962940fab138611dcca6ac6102d
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 13:53:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,28 +11,52 @@ df = pd.DataFrame(data)
 st.markdown(
     """
     <style>
-        .scrollable-table {
+        .scrollable-table-container {
             position: fixed;
-            top: 150px;
+            top: 50px;
             left: 0;
             right: 0;
             bottom: 0;
             overflow-y: auto;
         }
 
-        .scrollable-table table {
+        .scrollable-table-container table {
             width: 100%;
         }
 
-        .scrollable-table thead {
+        .scrollable-table-container td, .scrollable-table-container th {
+            white-space: nowrap;
+        }
+
+        .fixed-table {
+            position: fixed;
+            top: 0;
+            left: 0;
+            right: 0;
+            z-index: 1;
+        }
+
+        .fixed-table thead tr {
             position: sticky;
             top: 0;
+        }
+
+        .fixed-table th {
+            background-color: white;
         }
     </style>
     """,
     unsafe_allow_html=True
 )
 
+# 分离表头和数据
+header = df.iloc[:1]
+data = df.iloc[1:]
+
 # 显示表格
 with st.beta_container():
-    st.write("<div class='scrollable-table'>", df.to_html(index=False), "</div>", unsafe_allow_html=True)
+    # 固定表头
+    st.write("<div class='fixed-table'>", header.to_html(index=False), "</div>", unsafe_allow_html=True)
+
+    # 可滚动数据
+    st.write("<div class='scrollable-table-container'>", data.to_html(index=False), "</div>", unsafe_allow_html=True)
