0f7b35abc548ec49f656b906bd251e89ad10b500
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 17:45:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,8 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '性别': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 将DataFrame转换为Styler对象，并添加CSS样式
-styled_df = df.style.set_table_styles([{
-    'selector': 'thead th',
-    'props': [('position', 'sticky'), ('top', '0'), ('background-color', 'white')]
-}])
+# 设置首行冻结样式
+styles = [dict(selector="th", props=[("position", "sticky"), ("top", "0"), ("background-color", "white")])]
 
-# 在Streamlit中显示表格
-st.table(styled_df)
+# 显示表格
+st.table(df.style.set_table_styles(styles).set_properties(**{'position': 'sticky', 'top': '0'}))
