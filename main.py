aebb0f18ba04415b10f6ee7b857871146fac1deb
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 17:08:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,48 @@
 import streamlit as st
 import pandas as pd
 
-data = {'姓名': ['小明', '小红', '小刚']*100,
-        '年龄': [18, 19, 20]*100,
-        '性别': ['男', '女', '男']*100}
+# 创建示例数据
+data = {'姓名': ['小明', '小红', '小刚'] * 100,
+        '年龄': [18, 19, 20] * 100,
+        '性别': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-styles = [
-    dict(selector='th', props=[('text-align', 'center')]),
-    dict(selector='td', props=[('text-align', 'center')])
-]
+# 定义CSS样式
+css = """
+<style>
+table.dataframe {
+    width: 100%;
+    border-collapse: collapse;
+}
 
-styled_table = df.style.set_table_styles(styles)
+table.dataframe th {
+    position: sticky;
+    top: 0;
+    background: white;
+    border: 1px solid #d3d3d3;
+    text-align: center;
+    padding: 5px 0;
+}
 
-st.write(styled_table)
+table.dataframe th:first-child {
+    left: 0;
+    z-index: 1;
+}
+
+table.dataframe td {
+    border: 1px solid #d3d3d3;
+    text-align: center;
+    padding: 5px 0;
+}
+
+div.stTableContainer {
+    height: 600px;
+    overflow-y: scroll;
+}
+
+</style>
+"""
+
+# 显示数据
+st.markdown(css, unsafe_allow_html=True)
+st.table(df)
