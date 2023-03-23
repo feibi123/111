a532a74c9311dce0023386c901728e3d53d3101e
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 16:40:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,33 +7,35 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '性别': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 设置表格样式
+# 在CSS中设置表格样式
 st.markdown(
     """
     <style>
-        /* 设置表格样式 */
-        .scrollable-table {
-            max-height: 600px;
-            overflow-y: scroll;
-        }
-        /* 设置表头样式 */
-        .scrollable-table th {
-            position: sticky;
-            top: 0;
-            background-color: white;
-        }
-        /* 设置首行样式 */
-        .scrollable-table tr:first-child {
-            position: sticky;
-            top: 38px; /* 设置表头高度 */
-            background-color: white;
-            z-index: 1;
-        }
+      /* 表格样式 */
+      table {
+        width: 100%;
+        border-collapse: collapse;
+      }
+
+      /* 表头样式 */
+      table th {
+        position: sticky;
+        top: 0;
+        background-color: white;
+        font-weight: bold;
+      }
+
+      /* 首列样式 */
+      table th:first-child,
+      table td:first-child {
+        position: sticky;
+        left: 0;
+        background-color: white;
+      }
     </style>
     """,
     unsafe_allow_html=True
 )
 
 # 显示表格
-with st.beta_container():
-    st.write("<div class='scrollable-table'>", df.to_html(index=False), "</div>", unsafe_allow_html=True)
+st.write(df)
