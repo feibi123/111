a43ba81ecb7661bcf3205792e47068aeff4df457
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 13:43:15 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,3 +30,28 @@ st.markdown(
 # 显示表格
 with st.beta_container():
     st.write("<div style='overflow-x: auto;'><table id='mytable'>", df.to_html(index=False), "</table></div>", unsafe_allow_html=True)
+
+# 首行固定
+st.markdown(
+    """
+    <script>
+        var tableOffset = $("#mytable").offset().top;
+        var $header = $("#mytable > thead").clone();
+        var $fixedHeader = $("#header-fixed").append($header);
+        
+        $(window).bind("scroll", function() {
+            var offset = $(this).scrollTop();
+            if (offset >= tableOffset && $fixedHeader.is(":hidden")) {
+                $fixedHeader.show();
+            }
+            else if (offset < tableOffset) {
+                $fixedHeader.hide();
+            }
+        });
+    </script>
+    """,
+    unsafe_allow_html=True
+)
+
+# 创建空的表格，用于显示固定的表头
+st.markdown("<table id='header-fixed'></table>", unsafe_allow_html=True)
