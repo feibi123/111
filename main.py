1eb8f585419b26778740c2e9526f6c3933a86ff5
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 17:21:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,32 +7,13 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '性别': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 定义 CSS 样式
-st.markdown(
-    """
-    <style>
-        /* 设置表格样式 */
-        .scrollable-table {
-            height: 400px;
-            overflow-y: scroll;
-        }
-        /* 设置表头样式 */
-        .scrollable-table th {
-            position: sticky;
-            top: 0;
-            background-color: white;
-        }
-        /* 设置首行样式 */
-        .scrollable-table tbody tr:first-child {
-            position: sticky;
-            top: 0;
-            background-color: white;
-            font-weight: bold;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+# 冻结表格的首行
+def freeze_table(df):
+    # 获取表格HTML代码
+    table_html = df.to_html(index=False, classes=['freeze'])
+    # 在表格HTML代码中添加CSS样式
+    styled_html = '<style>.freeze tbody tr:first-child {position: sticky; top: 0; background-color: white;}</style>' + table_html
+    return styled_html
 
-# 显示表格
-st.table(df.style.set_table_attributes('class="scrollable-table"').render())
+# 显示冻结首行的表格
+st.write(freeze_table(df), unsafe_allow_html=True)
