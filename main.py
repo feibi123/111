a8f25e508a3b48bb6c8781b70a86ede123c43022
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 16:58:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-# 创建示例数据
-data = {'姓名': ['小明', '小红', '小刚'] * 100,
-        '年龄': [18, 19, 20] * 100,
-        '性别': ['男', '女', '男'] * 100}
+data = {'姓名': ['小明', '小红', '小刚'],
+        '年龄': [18, 19, 20],
+        '性别': ['男', '女', '男']}
 df = pd.DataFrame(data)
 
-# 冻结表头
-css = """
-    <style>
-        .freeze {
-            position: sticky !important;
-            top: 0;
-            background-color: white;
-            z-index: 999;
-        }
-        .freeze td {
-            font-weight: bold;
-        }
-        .freeze th {
-            font-weight: bold;
-        }
-    </style>
-"""
-st.markdown(css, unsafe_allow_html=True)
-df_html = df.to_html(classes=["freeze"], index=False)
+styles = [
+    dict(selector='th', props=[('text-align', 'center')]),
+    dict(selector='td', props=[('text-align', 'center')])
+]
 
-# 显示表格
-st.table(df_html, unsafe_allow_html=True)
+styled_table = df.style.set_table_styles(styles)
+
+st.write(styled_table)
