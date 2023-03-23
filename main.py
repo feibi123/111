5a5ff6f3151e6e3b3d33a878e85f4d23ef9456c5
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 11:15:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
+# import streamlit as st
+# import pandas as pd
+# uploaded_file1 = st.sidebar.file_uploader("上传订单报告")
+#  # 将解码后的文件内容转换为 pandas 数据框
+# df = pd.read_csv(uploaded_file1, skiprows=7)
+# df = df.dropna(subset=['quantity'])
+
+# st.table(df)
 import streamlit as st
 import pandas as pd
-from io import StringIO
-uploaded_file1 = st.sidebar.file_uploader("上传订单报告")
- # 将解码后的文件内容转换为 pandas 数据框
-df = pd.read_csv(uploaded_file1, skiprows=7)
-df = df.dropna(subset=['quantity'])
-styled_table = (
-        df.style
-          .set_table_styles([{'selector': 'thead th', 'props': [('position', 'sticky'), ('top', '0')] }])
-    )
 
-# 在 Streamlit 中显示表格
-st.table(styled_table)
+# 创建示例数据
+data = {'A': [1, 2, 3, 4, 5],
+        'B': [10, 20, 30, 40, 50],
+        'C': [100, 200, 300, 400, 500]}
+df = pd.DataFrame(data)
+
+# 使用st.markdown函数添加HTML标记
+st.markdown(
+    """
+    <style>
+        /* 设置表头的位置为固定 */
+        .freeze {
+            position: sticky;
+            top: 0;
+            z-index: 1;
+            background-color: white;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# 添加表格并将表头放置在一个固定的div元素中
+with st.container():
+    # 将表头放置在一个固定的div元素中
+    st.markdown(
+        f'<div class="freeze">{df.columns[0]}</div>',
+        unsafe_allow_html=True
+    )
+    # 显示数据表格
+    st.dataframe(df)
