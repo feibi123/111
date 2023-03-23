8eca69ef77ae5db39b18687d9a51273012c92ec1
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 14:31:57 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,56 +2,40 @@ import streamlit as st
 import pandas as pd
 
 # 创建示例数据
-data = {'Name': ['John', 'Emily', 'Michael'] * 100,
-        'Age': [25, 30, 35] * 100,
-        'Gender': ['M', 'F', 'M'] * 100}
+data = {'姓名': ['小明', '小红', '小刚'] * 100,
+        '年龄': [18, 19, 20] * 100,
+        '性别': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
+
+# 设置表格样式
+st.markdown(
+    """
+    <style>
+        .freeze-table-wrapper {
+            position: fixed;
+            top: 0;
+            width: 100%;
+            height: 100%;
+            overflow-x: hidden;
+            overflow-y: auto;
+        }
+
+        .freeze-table {
+            width: max-content;
+            margin-top: 45px;
+        }
+
+        .freeze-table th {
+            position: sticky;
+            top: 0;
+            background-color: white;
+            z-index: 1;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 # 显示表格
 with st.beta_container():
-    # 设置表格样式
-    st.markdown(
-        """
-        <style>
-            .scrollable-table {
-                position: fixed;
-                top: 80px;
-                left: 0;
-                width: 100%;
-                height: calc(100% - 80px);
-                z-index: 9999;
-                overflow: auto;
-            }
-
-            .scrollable-table table {
-                width: 100%;
-            }
-            
-            .scrollable-table thead th {
-                position: sticky;
-                top: 0;
-                background-color: white;
-            }
-            
-            .scrollable-table thead th:first-child {
-                left: 0;
-                z-index: 1;
-            }
-            
-            .scrollable-table thead th:nth-child(2) {
-                left: 120px;
-            }
-            
-            .scrollable-table thead th:nth-child(3) {
-                left: 200px;
-            }
-        </style>
-        """,
-        unsafe_allow_html=True
-    )
-    
-    st.write("<div class='scrollable-table'>", df.iloc[1:].to_html(index=False), "</div>", unsafe_allow_html=True)
-
-    # 显示表头
-    st.write("<div class='scrollable-table'>", df.iloc[:1].to_html(index=False), "</div>", unsafe_allow_html=True)
-
+    st.write("<div class='freeze-table-wrapper'>", "<table class='freeze-table'>", df.to_html(index=False), "</table>", "</div>", unsafe_allow_html=True)
