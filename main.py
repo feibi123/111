e394856841288378ba61c452fc0296f1e540fc43
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 14:33:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,34 @@ import streamlit as st
 import pandas as pd
 
 # 创建示例数据
-data = {'姓名': ['小明', '小红', '小刚'] * 100,
-        '年龄': [18, 19, 20] * 100,
-        '性别': ['男', '女', '男'] * 100}
+data = {'Name': ['小明', '小红', '小刚'] * 100,
+        'Age': [18, 19, 20] * 100,
+        'Gender': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
 # 设置表格样式
 st.markdown(
     """
     <style>
-        .freeze-table-wrapper {
+        .dataframe {
             position: fixed;
-            top: 0;
+            top: 70px;
             width: 100%;
             height: 100%;
-            overflow-x: hidden;
-            overflow-y: auto;
+            z-index: 0;
+            overflow: auto;
         }
 
-        .freeze-table {
-            width: max-content;
-            margin-top: 45px;
-        }
-
-        .freeze-table th {
+        .dataframe thead {
             position: sticky;
             top: 0;
             background-color: white;
             z-index: 1;
+        }
+
+        .dataframe th {
+            font-weight: bold;
+            text-align: center;
         }
     </style>
     """,
@@ -38,4 +38,4 @@ st.markdown(
 
 # 显示表格
 with st.beta_container():
-    st.write("<div class='freeze-table-wrapper'>", "<table class='freeze-table'>", df.to_html(index=False), "</table>", "</div>", unsafe_allow_html=True)
+    st.table(df)
