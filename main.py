00a4b93333bab30684d7bb52dedd547f4778e4ef
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-23 16:42:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+pd.set_option('display.max_colwidth', None)
+st.set_page_config(layout="wide")
+
 # 创建示例数据
 data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '年龄': [18, 19, 20] * 100,
