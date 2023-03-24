ac51645962d553bc9a43eccb0345c3f2370dbb3f
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-24 14:18:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,37 @@
-from st_aggrid import GridOptionsBuilder, AgGrid
 import streamlit as st
 import pandas as pd
+from st_aggrid import GridOptionsBuilder, AgGrid
 
-df = pd.DataFrame({'name': ['Alice', 'Bob', 'Charlie'], 'age': [25, 32, 18]})
+# 创建一个包含随机数据的 DataFrame
+df = pd.DataFrame({
+    '姓名': ['小明', '小红', '小刚'] * 100,
+    '年龄': [18, 19, 20] * 100,
+    '性别': ['男', '女', '男'] * 100
+})
 
-# 计算屏幕宽度的 90%
-screen_width = st.experimental_get_query_params()['screenWidth'][0]
-table_width = int(0.9 * float(screen_width))
-
-# 创建 GridOptionsBuilder 对象
+# 创建一个 GridOptionsBuilder 对象并从 DataFrame 中获取选项
 gb = GridOptionsBuilder.from_dataframe(df)
 
-# 设置表格宽度
-gb.with_grid_width(f"{table_width}px")
+# 配置表格选项
+gb.configure_default_column(
+    groupable=True,
+    value=True,
+    enableRowGroup=True,
+    aggFunc='sum',
+    editable=True
+)
 
 # 构建表格选项
 go = gb.build()
+
+# 获取屏幕宽度
+screen_width = st.experimental_get_query_params().get('screenWidth', [None])[0]
+
+# 设置表格宽度
+if screen_width:
+    go.with_dimensions(width=screen_width)
+else:
+    go.with_full_width()
 
 # 使用 AgGrid 组件来呈现表格
 AgGrid(df, gridOptions=go)
