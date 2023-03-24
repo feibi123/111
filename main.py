8e50a8ab54b1c716036600c825b63d7fe43fc96b
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-24 10:23:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,13 @@
 import streamlit as st
+import streamlit_aggrid as st_ag
 import pandas as pd
 
-# Create example data
-data = {'Name': ['Alice', 'Bob', 'Charlie'] * 100,
-        'Age': [25, 30, 35] * 100,
-        'Gender': ['F', 'M', 'M'] * 100}
-df = pd.DataFrame(data)
+df = pd.DataFrame({
+   'name': ['Alice', 'Bob', 'Charlie', 'David'],
+   'age': [25, 32, 18, 47],
+   'city': ['New York', 'Paris', 'London', 'Tokyo'],
+   'country': ['USA', 'France', 'UK', 'Japan']
+})
 
-# Define CSS and JavaScript to freeze first row of table and synchronize scrolling with page
-freeze_table_script = """
-    <style>
-        /* Add custom styles for first row */
-        .freeze-table th:first-child, .freeze-table td:first-child {
-            position: sticky;
-            left: 0;
-            z-index: 1;
-            background-color: white;
-        }
-        /* Add custom styles for remaining rows */
-        .freeze-table th, .freeze-table td {
-            background-color: white;
-        }
-    </style>
-    <script>
-        // Synchronize scrolling of table with page
-        window.onscroll = function() {
-            var scrollPosition = window.pageYOffset;
-            var tableTop = document.getElementById('table').getBoundingClientRect().top + window.pageYOffset;
-            var tableBottom = tableTop + document.getElementById('table').clientHeight;
-            var windowHeight = window.innerHeight;
-            if (scrollPosition > tableTop - windowHeight/2 && scrollPosition < tableBottom - windowHeight/2) {
-                document.getElementById('table').style.transform = 'translate(0px,' + (scrollPosition - tableTop + windowHeight/2) + 'px)';
-            }
-        };
-    </script>
-"""
-
-# Display table with frozen first row and synchronized scrolling
-with st.beta_container():
-    st.markdown(freeze_table_script, unsafe_allow_html=True)
-    st.dataframe(df.style.set_table_attributes('class="freeze-table" id="table"').hide_index())
-
+grid_height = 500
+st_ag.grid(df, height=grid_height, fit_columns_on_grid_load=True)
