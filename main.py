9bbf2b62efae6c7fc78655c2aece0c6caf192d95
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-24 13:14:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,1 @@
-import streamlit as st
-import streamlit_aggrid as st_ag
-import pandas as pd
 
-df = pd.DataFrame({
-   'name': ['Alice', 'Bob', 'Charlie', 'David'],
-   'age': [25, 32, 18, 47],
-   'city': ['New York', 'Paris', 'London', 'Tokyo'],
-   'country': ['USA', 'France', 'UK', 'Japan']
-})
-
-grid_height = 500
-st_ag.grid(df, height=grid_height, fit_columns_on_grid_load=True)
