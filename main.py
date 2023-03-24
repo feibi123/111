61a94da0120ff82b2ce61c5dbbf542ffd7feceb9
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-24 14:14:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import pandas as pd
 from st_aggrid import GridOptionsBuilder, AgGrid
 
 # 创建一个包含随机数据的 DataFrame
-df = pd.DataFrame({
-    '姓名': ['小明', '小红', '小刚'] * 100,
-    '年龄': [18, 19, 20] * 100,
-    '性别': ['男', '女', '男'] * 100
-})
+df = pd.DataFrame( {'姓名': ['小明', '小红', '小刚'] * 100,
+        '年龄': [18, 19, 20] * 100,
+        '性别': ['男', '女', '男'] * 100})
 
 # 创建一个 GridOptionsBuilder 对象并从 DataFrame 中获取选项
 gb = GridOptionsBuilder.from_dataframe(df)
@@ -20,11 +18,14 @@ gb.configure_default_column(
     editable=True
 )
 
-# 使用 with_dimensions 方法来设置表格宽度和高度
-gb.configure_grid_options(domLayout='normal', with_dimensions=True)
+# 设置自动调整表格大小
+gb.configure_grid_options(
+    autoHeight=True,
+    autoWidth=True
+)
 
 # 构建表格选项
 go = gb.build()
 
 # 使用 AgGrid 组件来呈现表格
-AgGrid(df, gridOptions=go, height=500, width='100%')
+AgGrid(df, gridOptions=go)
