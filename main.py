cd9452caf31a2fd9d91f1ffe54bf331d5cce030e
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-24 13:57:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,43 +1,20 @@
 import streamlit as st
-import pandas as pd
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode
 
-# 创建示例数据
-data = {'姓名': ['小明', '小红', '小刚'] * 100,
-        '年龄': [18, 19, 20] * 100,
-        '性别': ['男', '女', '男'] * 100}
-df = pd.DataFrame(data)
+# 创建一个 GridOptionsBuilder 对象
+gb = GridOptionsBuilder.from_dataframe(df)
 
-# 设置表格样式
-st.markdown(
-    """
-    <style>
-        /* 设置表格样式 */
-        .scrollable-table {
-            height: 600px;
-            overflow-y: scroll;
-            position: sticky;
-            top: 0;
-            background-color: white;
-            z-index: 1;
-        }
-
-        /* 设置表头样式 */
-        .scrollable-table th {
-            position: sticky;
-            top: 0;
-            background-color: white;
-            z-index: 2;
-        }
-
-        /* 设置表格行的样式 */
-        .scrollable-table tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
+# 设置表格高度和宽度
+gb.configure_default_column(
+    groupable=True,
+    value=True,
+    enableRowGroup=True,
+    aggFunc='sum',
+    editable=True
 )
 
-# 显示表格
-with st.beta_container():
-    st.write("<div class='scrollable-table'>", df.to_html(index=False), "</div>", unsafe_allow_html=True)
+# 构建表格选项
+go = gb.build()
+
+# 使用 AgGrid 组件来呈现表格
+AgGrid(df, gridOptions=go, height=500, width=800)
