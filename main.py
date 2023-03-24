61a6696403766d67e62624c8548492335f939ef4
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-24 14:17:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
-import pandas as pd
 from st_aggrid import GridOptionsBuilder, AgGrid
 import streamlit as st
+import pandas as pd
 
-# 创建一个包含随机数据的 DataFrame
-df = pd.DataFrame( {'姓名': ['小明', '小红', '小刚'] * 100,
-        '年龄': [18, 19, 20] * 100,
-        '性别': ['男', '女', '男'] * 100})
+df = pd.DataFrame({'name': ['Alice', 'Bob', 'Charlie'], 'age': [25, 32, 18]})
 
+# 计算屏幕宽度的 90%
 screen_width = st.experimental_get_query_params()['screenWidth'][0]
 table_width = int(0.9 * float(screen_width))
 
-# 创建一个 GridOptionsBuilder 对象并从 DataFrame 中获取选项
+# 创建 GridOptionsBuilder 对象
 gb = GridOptionsBuilder.from_dataframe(df)
 
+# 设置表格宽度
 gb.with_grid_width(f"{table_width}px")
 
 # 构建表格选项
