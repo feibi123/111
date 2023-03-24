2326f7ed8f720bcc1122c73c4b27fd80aff61d35
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-24 14:10:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import pandas as pd
 from st_aggrid import GridOptionsBuilder, AgGrid
 
 # 创建一个包含随机数据的 DataFrame
-df = pd.DataFrame({
-    'A': [1, 2, 3, 4],
-    'B': [5, 6, 7, 8],
-    'C': [9, 10, 11, 12]
-})
+df = pd.DataFrame( {'姓名': ['小明', '小红', '小刚'] * 100,
+        '年龄': [18, 19, 20] * 100,
+        '性别': ['男', '女', '男'] * 100})
 
 # 创建一个 GridOptionsBuilder 对象并从 DataFrame 中获取选项
 gb = GridOptionsBuilder.from_dataframe(df)
