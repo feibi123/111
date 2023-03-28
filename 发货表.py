aa03e429b852f42c5c27689c26badd688e978901
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 13:12:51 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,9 +1,8 @@
 import streamlit.components.v1 as components
-from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid import AgGrid
 import pandas as pd
 import streamlit as st
 from io import StringIO
-pd.set_option('display.max_colwidth', None)
 st.set_page_config(layout="wide")
 
 uploaded_file1 = st.sidebar.file_uploader("上传订单报告")
@@ -30,13 +29,31 @@ df = df.dropna(subset=['quantity'])  # 删除含有空值的行
 df['quantity'] = df['quantity'].astype(int)  # 将quantity列转换成整数类型
 df = df.dropna(subset=['type'])   # 删除含有空值的行
 
-gb = GridOptionsBuilder.from_dataframe(df)
+grid_options = {
+    'columnDefs': [{'field': col, 'sortable': True} for col in df.columns],
+    'rowSelection': 'single',
+    'floatingFilter': True,  # 冻结首行
+    'ensureDomOrder': True,  # 优化渲染性能
+    'enableFilter': True,
+    'enableSorting': True,
+    'enableColResize': True,
+    'enableRangeSelection': True,
+    'pagination': True,
+    'paginationAutoPageSize': True,
+    'suppressMenuFilterPanel': True,
+    'suppressContextMenu': True,
+    'domLayout': 'normal',  # 设置为normal模式
+    'overlayNoRowsTemplate': '没有数据',
+}
 
-# 冻结首行
-gb.configure_grid_options(domLayout='normal')
-gb.configure_column("index", headerName="", maxWidth=50, lockPosition=True)
+grid_width = len(df.columns) * 200  # 根据列数计算表格宽度
 
-gridOptions = gb.build()
+with st.spinner('正在加载数据...'):grid = AgGrid(
+        df,
+        gridOptions=grid_options,
+        width=grid_width,
+        theme='light',
+        allow_unsafe_jscode=True,  # 允许渲染JS代码，防止组件出错
+    )
 
-# 使用 AgGrid 组件展示数据
-grid = AgGrid(df, gridOptions=gridOptions, height=600)
+st.markdown("<style>div.row-widget.stRadio > div{flex-direction:row;}</style>", unsafe_allow_html=True)
