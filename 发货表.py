070614622fa750c2111bd33d3516a718556c6ff6
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 12:48:05 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,11 +1,30 @@
 import streamlit.components.v1 as components
 from st_aggrid import AgGrid
 import pandas as pd
+import streamlit as st
 
-# 创建示例数据
-data = {'姓名': ['小明', '小红', '小刚'] * 100,
-        '年龄': [18, 19, 20] * 100,
-        '性别': ['男', '女', '男'] * 100}
+uploaded_file1 = st.sidebar.file_uploader("上传订单报告")
+
+# 如果用户上传了文件
+if uploaded_file1 is not None:
+    # 读取文件内容
+    content = uploaded_file1.read()
+
+    # 尝试使用 utf-8 编码方式进行解码
+    try:
+        decoded_content = content.decode('utf-8')
+    except UnicodeDecodeError:
+        # 如果解码失败，则尝试使用 gbk 编码方式进行解码
+        decoded_content = content.decode('gbk')
+
+    # 将解码后的文件内容转换为 pandas 数据框
+    df = pd.read_csv(StringIO(decoded_content), skiprows=7)
+
+    
+df = df.dropna(subset=['quantity'])  # 删除含有空值的行
+df['quantity'] = df['quantity'].astype(int)  # 将quantity列转换成整数类型
+df = df.dropna(subset=['type']) 
+
 df = pd.DataFrame(data)
 
 # 使用 AgGrid 组件展示数据
