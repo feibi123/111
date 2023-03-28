fd838e4a1d25af5ef2ad5961fc8c3369086aa23f
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 14:33:28 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,37 +1,24 @@
-import streamlit.components.v1 as components
-from st_aggrid import AgGrid, GridOptionsBuilder
-import pandas as pd
 import streamlit as st
-from io import StringIO
-pd.set_option('display.max_colwidth', None)
-st.set_page_config(layout="wide")
+import pandas as pd
+from st_aggrid import AgGrid, GridOptionsBuilder
 
-uploaded_file1 = st.sidebar.file_uploader("上传订单报告")
-df = pd.DataFrame()
+# 创建数据
+data = {'姓名': ['小明', '小红', '小张', '小李', '小刚'], '语文': [78, 92, 85, 90, 87], '数学': [83, 76, 92, 88, 82],
+        '英语': [87, 85, 72, 90, 92]}
+df = pd.DataFrame(data)
 
+# 设置单元格样式
+def style_cell(x):
+    style = ''
+    if x < 80:
+        style += "font-weight: bold; color: red;"
+    elif x > 90:
+        style += "font-weight: bold; color: green;"
+    return style
 
-# 如果用户上传了文件
-if uploaded_file1 is not None:
-    # 读取文件内容
-    content = uploaded_file1.read()
-
-    # 尝试使用 utf-8 编码方式进行解码
-    try:
-        decoded_content = content.decode('utf-8')
-    except UnicodeDecodeError:
-        # 如果解码失败，则尝试使用 gbk 编码方式进行解码
-        decoded_content = content.decode('gbk')
-
-    # 将解码后的文件内容转换为 pandas 数据框
-    df = pd.read_csv(StringIO(decoded_content), skiprows=7)
-
-    
-df = df.dropna(subset=['quantity'])  # 删除含有空值的行
-df['quantity'] = df['quantity'].astype(int)  # 将quantity列转换成整数类型
-df = df.dropna(subset=['type'])   # 删除含有空值的行
-
-# 创建 GridOptionsBuilder 对象，用于构建 AgGrid 的参数
-gb = GridOptionsBuilder.from_dataframe(df)
+# 应用样式
+styled_df = df.style.applymap(style_cell)
+gb = GridOptionsBuilder.from_dataframe(styled_df)
 
 # 冻结首行
 gb.configure_grid_options(domLayout='normal')
@@ -43,4 +30,7 @@ gb.configure_grid_options(domLayout='autoHeight', widthMode='fit')
 gridOptions = gb.build()
 
 # 使用 AgGrid 组件展示数据
-grid = AgGrid(df, gridOptions=gridOptions, height=600)
+grid = AgGrid(styled_df, gridOptions=gridOptions, height=600)
+
+# 使用 st.write 显示 Pandas DataFrame
+st.write(df, wide=True)
