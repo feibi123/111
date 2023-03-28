7c0d8f40162c5a872e4a9d7652a4f20cae1f4cb5
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 13:53:42 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -39,6 +39,7 @@ gb.configure_column("index", headerName="", maxWidth=50, lockPosition=True)
 
 # 设置表格的宽度自适应页面的宽度
 gb.configure_grid_options(domLayout='autoHeight', widthMode='fit')
+
 gridOptions = gb.build()
 
 # 使用 AgGrid 组件展示数据
