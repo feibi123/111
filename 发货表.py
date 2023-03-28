9310df77ed1231d4aac1742150e7ccd7fcf57c16
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 12:32:46 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,4 +1,5 @@
-from st_aggrid import AgGrid
+from st_aggrid import GridOptionsBuilder, AgGrid
+
 import pandas as pd
 
 # 创建示例数据
@@ -6,5 +7,12 @@ data = {'name': ['Alice', 'Bob', 'Charlie', 'David'],
         'age': [25, 30, 35, 40]}
 df = pd.DataFrame(data)
 
+# 创建 GridOptionsBuilder 对象
+gb = GridOptionsBuilder.from_dataframe(df)
+
+# 配置 GridOptions
+gb.configure_pagination(paginationAutoPageSize=True)
+gb.configure_side_bar()
+
 # 使用 AgGrid 组件展示数据
-grid = AgGrid(df)
+grid = AgGrid(df, gridOptions=gb.build(), width='100%', height='400px')
