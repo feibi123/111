45dc7808e6544791e8f6b6bdea277bb8d837df1b
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 16:26:40 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -10,5 +10,11 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
+# 设置 defaultColumnDef 属性，使所有列名能够完全显示出来
+default_column_def = {'minWidth': 100}
+grid_options = {
+    'defaultColumnDef': default_column_def
+}
+
 # 使用 AgGrid 组件展示数据
-grid = AgGrid(df)
+grid = AgGrid(df, grid_options=grid_options)
