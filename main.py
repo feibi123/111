53c81f8faffa6f8ec81ab1c33b524c04fdb615f9
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 11:07:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,10 @@
-import streamlit as st
+from st_aggrid import AgGrid
 import pandas as pd
 
 # 创建示例数据
-data = {'姓名': ['小明', '小红', '小刚'] * 100,
-        '年龄': [18, 19, 20] * 100,
-        '性别': ['男', '女', '男'] * 100}
+data = {'name': ['Alice', 'Bob', 'Charlie', 'David'],
+        'age': [25, 30, 35, 40]}
 df = pd.DataFrame(data)
 
-# 设置表格样式
-st.markdown(
-    """
-    <style>
-        .scrollable-table {
-            position: fixed;
-            top: 150px;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            overflow-y: auto;
-        }
-
-        .scrollable-table table {
-            width: 100%;
-        }
-
-        .scrollable-table thead {
-            position: sticky;
-            top: 0;
-        }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
-
-# 显示表格
-with st.beta_container():
-    st.write("<div class='scrollable-table'>", df.to_html(index=False), "</div>", unsafe_allow_html=True)
+# 使用 AgGrid 组件展示数据
+grid = AgGrid(df)
