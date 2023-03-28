ab6456d4f30dc9b5250a2f1e35c6b06ef9943135
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 15:54:50 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -3,10 +3,10 @@ import pandas as pd
 import numpy as np
 
 # 创建一个包含随机数据的数据框
-df = pd.DataFrame(np.random.randn(10, 5), columns=['col1', 'col2', 'col3', 'col4', 'col5'])
+df = pd.DataFrame(np.random.randn(10, 5), columns=['col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7', 'col8', 'col9'])
 
 # 将数据框转换为 streamlit 表格
-st.table(df)
+table = st.table(df)
 
 # 使用 CSS 样式来设置表格铺满全屏并冻结首行
 st.markdown("""
@@ -21,10 +21,12 @@ st.markdown("""
             z-index: 0 !important;
         }
         
-        #root > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(2) > div > div > div > div:nth-child(1) > div {
+        #root > div:nth-child(1) > div > div:nth-child(1) > div:nth-child(2) > div > div > div:nth-child(1) > div > div {
             position: sticky !important;
             top: 0 !important;
             z-index: 1 !important;
+            background-color: #ffffff !important;
+            font-weight: bold !important;
         }
     </style>
 """, unsafe_allow_html=True)
