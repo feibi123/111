045450ea9f5cd79c0a338d971a53e22ad618de26
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 16:57:04 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,8 +1,7 @@
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder
 import pandas as pd
+from st_aggrid import AgGrid
 
-# 创建示例数据
 data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '年龄': [18, 19, 20] * 100,
         '性别': ['男', '女', '男'] * 100,
@@ -10,17 +9,31 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 设置页面宽度和高度
-st.set_page_config(page_title="AgGrid Example", layout="wide")
+st.write("""
+<style>
+#grid-container {
+    height: 500px; /* 设置 AgGrid 的高度 */
+    overflow: auto; /* 显示页面滚动条 */
+}
+#grid-frozen-rows {
+    position: sticky; /* 固定在顶部 */
+    top: 0; /* 距离顶部的位置 */
+}
+</style>
+""", unsafe_allow_html=True)
 
-# 创建 GridOptionsBuilder 对象
-gb = GridOptionsBuilder.from_dataframe(df)
-
-# 设置 AgGrid 组件的属性
-gridOptions = gb.build()
-gridOptions['floatingTopRow'] = {'data': [{'姓名': '', '年龄': '', '性别': '', '身高': '', '体重': ''}], 'rowHeight': 40}
-gridOptions['enablePagination'] = False
-gridOptions['onGridReady'] = "function(params) {params.api.setDomLayout('normal');}"
+# 设置 AgGrid 组件的宽度
+grid_options = {
+    'width': '100%',
+    'enableSorting': True, # 开启排序
+    'enableFilter': True, # 开启过滤
+    'enableColResize': True, # 开启调整列宽
+    'floatingTopRow': True, # 启用冻结行
+    'floatingTopRowData': [df.columns.tolist()], # 冻结首行
+}
 
 # 使用 AgGrid 组件展示数据
-grid_response = AgGrid(df, gridOptions=gridOptions, height=600, width='100%')
+grid = AgGrid(df, grid_options=grid_options, key='grid')
+
+# 如果您的数据集非常大，可以使用 pagination_mode='virtual' 来开启虚拟分页，以加速渲染速度
+# grid = AgGrid(df, grid_options=grid_options, key='grid', pagination_mode='virtual')
