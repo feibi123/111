5f5c6f3e89dcb853928460d76dfaea930d24a28b
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 14:01:03 +0800

    Update streamlit数据可视化.py

--- streamlit数据可视化.py
+++ streamlit数据可视化.py
@@ -159,7 +159,8 @@ def style_cell1(y):
 
 # 应用样式
 styled_df = df.style.applymap(style_cell1, subset=['最晚发货时间'])
-df = styled_df.applymap(style_cell, subset=['在库预计可售天数', '总预计可售天数'])
+styled_data = styled_df.data
+df = styled_data.applymap(style_cell, subset=['在库预计可售天数', '总预计可售天数'])
 
 gb = GridOptionsBuilder.from_dataframe(df)
 
