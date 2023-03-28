75e63377fa105ccf6cd64251494087a2daf81ef3
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 16:29:37 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import streamlit.components.v1 as components
 from st_aggrid import AgGrid
 import pandas as pd
@@ -10,10 +11,12 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 设置 defaultColumnDef 属性，使所有列名能够完全显示出来
-default_column_def = {'minWidth': 50}
+# 设置页面宽度和高度
+st.set_page_config(page_title="AgGrid Example", layout="wide")
+
+# 设置 AgGrid 组件的宽度
 grid_options = {
-    'defaultColumnDef': default_column_def
+    'width': '100%',
 }
 
 # 使用 AgGrid 组件展示数据
