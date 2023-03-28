3e7516e3a68648a8dee388ab0bd7a751e6d033fa
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-28 15:26:57 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,21 +1,19 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_excel(r"E:\发货表\FSM\1.xlsx")
+data = {
+    '姓名': ['小明', '小红', '小张', '小李', '小刚'],
+    '语文': [78, 92, 85, 90, 87],
+    '数学': [83, 76, 92, 88, 82],
+    '英语': [87, 85, 72, 90, 92]
+}
 
-# 冻结表格首行
-st.write(
-    df.style
-    .set_table_styles([{'selector': 'thead', 'props': [('sticky', 'top')]}])
-    .set_properties(**{'width': '100%', 'max-width': '100%'})
-)
+df = pd.DataFrame(data)
+
+st.write(df.style.highlight_max(axis=0))
+
+# 冻结表头
+st.write(df.style.highlight_max(axis=0).set_table_styles([{'selector': 'thead th', 'props': [('position', 'sticky'), ('top', '0px')]}]))
 
 # 铺满全屏
-st.markdown("""
-    <style>
-        .fullScreen {
-            height: 100vh !important;
-        }
-    </style>
-""", unsafe_allow_html=True)
-
+st.write(df.style.set_table_styles([{'selector': 'table', 'props': [('width', '100%'), ('height', '100%')]}]))
