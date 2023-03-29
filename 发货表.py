28e3762f8c1f4f3a308c3261bac523cd231c0f67
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 15:21:17 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,25 +1,3 @@
-# import streamlit as st
-# from st_aggrid import AgGrid, GridOptionsBuilder
-# import pandas as pd
-# # st.set_page_config(page_title="AgGrid Example", layout="wide")
-# # 创建示例数据
-# data = {'姓名': ['小明', '小红', '小刚'] * 100,
-#         '年龄': [18, 19, 20] * 100,
-#         '性别': ['男', '女', '男'] * 100,
-#         '身高': ['男', '女', '男'] * 100,
-#         '体重': ['男', '女', '男'] * 100}
-# df = pd.DataFrame(data)
-
-# 创建 GridOptionsBuilder 对象
-# gb = GridOptionsBuilder.from_dataframe(df)
-
-# 设置 AgGrid 组件的属性
-# gridOptions = gb.build()
-# gridOptions['onGridReady'] = "function(params) {params.api.setDomLayout('normal');params.api.sizeColumnsToFit();}"
-# gridOptions['defaultColDef'] = {'flex': 1}
-
-# 使用 AgGrid 组件展示数据
-# grid_response = AgGrid(df, gridOptions=gridOptions, height=600, width='100%')
 import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder
 import pandas as pd
@@ -44,13 +22,6 @@ gridOptions['onGridReady'] = "function(params) {params.api.setDomLayout('normal'
 gridOptions['defaultColDef'] = {'flex': 1}
 
 window_height = st.experimental_get_query_params().get('height', [None])[0]
-
-
-# # 转换高度为整数值
-# if window_height:
-#     window_height = int(window_height.replace("px", ""))
-# else:
-#     window_height = None
 
 window_width = '100%'
 # 使用 AgGrid 组件展示数据
