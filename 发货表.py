59bb41aa686c0f20b5d5b513b3af2e66a205d2ad
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 10:26:06 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -9,4 +9,7 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-st.table(df)
+# 在Streamlit中显示表格，并将表格全屏并冻结首行
+st.write(df.style.set_table_styles(
+    [{"selector": "thead", "props": [("position", "sticky"), ("top", "0px")]}]
+).set_properties(**{'width': '100%', 'height': '100vh', 'max-height': 'none', 'overflow-y': 'auto'}))
