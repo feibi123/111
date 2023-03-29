a1d42a52bf97058bd32c3d911e5fca08509bbeaf
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 12:53:27 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -9,11 +9,19 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 设置表头固定
-styles = [dict(selector="thead", props=[("position", "sticky"), ("top", "0px")])]
+# 设置表头固定和滚动
+st.markdown("""
+    <style>
+    .freeze-table {
+      overflow: auto;
+      max-height: 600px;
+    }
+    .freeze-table thead {
+      position: sticky;
+      top: 0;
+      background-color: white;
+    }
+    </style>
+""", unsafe_allow_html=True)
 
-# 使用 Styler 将 DataFrame 转化为 HTML 格式
-html = df.style.set_table_styles(styles).render()
-
-# 渲染 HTML 字符串
-st.markdown(html, unsafe_allow_html=True)
+st.table(df.style.set_properties(**{'width': '100%', 'max-height': '500px', 'overflow-y': 'auto'}).set_table_attributes("class='freeze-table'"))
