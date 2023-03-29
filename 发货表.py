1b5a21320a197abb37f8f9c71d3d966bcf9b2f5e
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 13:03:10 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,6 +1,6 @@
-import pandas as pd
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+import pandas as pd
+from streamlit_aggrid import GridOptionsBuilder, AgGrid
 
 # 创建示例数据
 data = {'姓名': ['小明', '小红', '小刚'] * 100,
@@ -10,18 +10,44 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 创建 GridOptionsBuilder 对象
+# 创建 Ag-Grid 配置实例
 gob = GridOptionsBuilder.from_dataframe(df)
 
-# 固定表格首行
-gob.configure_default_column(groupable=False, value=True, enableRowGroup=True, aggFunc='sum', editable=True, resizable=True, filter=True, sortable=True, flex=1)
-gob.configure_grid_options(domLayout='normal', rowDragManaged=True, enableRangeSelection=True, enableCharts=True, enableStatusBar=True, enableFilter=True, enableSorting=True, enableColResize=True, enableRangeSelection=True, enableCharts=True, enableStatusBar=True, enableFilter=True, enableSorting=True, enableColResize=True, enableCellChangeFlash=True, suppressDragLeaveHidesColumns=True, rowSelection='single', rowMultiSelectWithClick=True, suppressRowClickSelection=False, groupSelectsChildren=True, groupSelectsFiltered=True, suppressAggFuncInHeader=True, suppressMultiSort=False, rowHeight=22, headerHeight=28, defaultColDef={'sortable': True})
+# 设置列宽和行高
+gob.configure_column("姓名", width=120)
+gob.configure_column("年龄", width=80)
+gob.configure_column("性别", width=80)
+gob.configure_column("身高", width=80)
+gob.configure_column("体重", width=80)
 
-# 在页面上显示表格
-grid_result = AgGrid(df, gridOptions=gob.build(), width='100%', height='500px', theme='streamlit', update_mode=GridUpdateMode.VALUE_CHANGED)
+gob.configure_grid_options(domLayout='normal',
+                           rowDragManaged=True,
+                           enableRangeSelection=True,
+                           enableCharts=True,
+                           enableStatusBar=True,
+                           enableFilter=True,
+                           enableSorting=True,
+                           enableColResize=True,
+                           enableCellChangeFlash=True,
+                           suppressDragLeaveHidesColumns=True,
+                           rowSelection='single',
+                           rowMultiSelectWithClick=True,
+                           suppressRowClickSelection=False,
+                           groupSelectsChildren=True,
+                           groupSelectsFiltered=True,
+                           suppressAggFuncInHeader=True,
+                           suppressMultiSort=False,
+                           rowHeight=22,
+                           headerHeight=28,
+                           defaultColDef={'sortable': True})
 
-# 获取用户所做的更改
-changed_df = grid_result['data']
+# 添加冻结首行
+gob.configure_grid_options(floatingFilter=True, 
+                           floatingTopRow=True, 
+                           suppressHorizontalScroll=True, 
+                           floatingTopRowData=df.head(1))
 
-# 输出更改后的结果
-st.write(changed_df)
+grid_response = AgGrid(df, gridOptions=gob.build(), height=500)
+
+# 显示表格
+st.write(grid_response['data'])
