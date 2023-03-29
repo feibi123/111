bb332672bb16da913580f951aeaa72bcb82bb143
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 10:25:01 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -9,6 +9,4 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-st.write(df.style.set_table_styles(
-    [{"selector": "thead", "props": [("position", "sticky"), ("top", "0px")]}]
-).set_properties(**{'width': '100%', 'max-height': '500px', 'overflow-y': 'auto'}))
+st.table(df)
