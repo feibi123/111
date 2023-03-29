77b26d85824e022c76d92c290b6fedf0006e6561
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 13:00:13 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 
 # 创建示例数据
 data = {'姓名': ['小明', '小红', '小刚'] * 100,
@@ -9,27 +10,18 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 设置表格样式
-table_style = """
-<style>
-table {
-  table-layout: fixed;
-}
-thead {
-  position: sticky;
-  top: 0;
-  background-color: white;
-}
-tbody {
-  overflow-y: scroll;
-  height: 400px;
-}
-th, td {
-  text-align: center;
-}
-</style>
-"""
+# 创建 GridOptionsBuilder 对象
+gob = GridOptionsBuilder.from_dataframe(df)
 
-# 显示表格
-st.markdown(table_style, unsafe_allow_html=True)
-st.markdown(df.to_html(index=False), unsafe_allow_html=True)
+# 固定表格首行
+gob.configure_default_column(groupable=False, value=True, enableRowGroup=True, aggFunc='sum', editable=True, resizable=True, filter=True, sortable=True, flex=1)
+gob.configure_grid_options(domLayout='normal', rowDragManaged=True, enableRangeSelection=True, enableCharts=True, enableStatusBar=True, enableFilter=True, enableSorting=True, enableColResize=True, enableRangeSelection=True, enableCharts=True, enableStatusBar=True, enableFilter=True, enableSorting=True, enableColResize=True, enableCellChangeFlash=True, suppressDragLeaveHidesColumns=True, rowSelection='single', rowMultiSelectWithClick=True, suppressRowClickSelection=False, groupSelectsChildren=True, groupSelectsFiltered=True, suppressAggFuncInHeader=True, suppressMultiSort=False, rowHeight=22, headerHeight=28, defaultColDef={'sortable': True})
+
+# 在页面上显示表格
+grid_result = AgGrid(df, gridOptions=gob.build(), width='100%', height='500px', theme='streamlit', update_mode=GridUpdateMode.VALUE_CHANGED)
+
+# 获取用户所做的更改
+changed_df = grid_result['data']
+
+# 输出更改后的结果
+st.write(changed_df)
