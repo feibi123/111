2d2f251d7481c6fa72220d25609c29c22f0e6013
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 14:44:12 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,22 +1,22 @@
-import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder
-import pandas as pd
-# st.set_page_config(page_title="AgGrid Example", layout="wide")
-# 创建示例数据
-data = {'姓名': ['小明', '小红', '小刚'] * 100,
-        '年龄': [18, 19, 20] * 100,
-        '性别': ['男', '女', '男'] * 100,
-        '身高': ['男', '女', '男'] * 100,
-        '体重': ['男', '女', '男'] * 100}
-df = pd.DataFrame(data)
+# import streamlit as st
+# from st_aggrid import AgGrid, GridOptionsBuilder
+# import pandas as pd
+# # st.set_page_config(page_title="AgGrid Example", layout="wide")
+# # 创建示例数据
+# data = {'姓名': ['小明', '小红', '小刚'] * 100,
+#         '年龄': [18, 19, 20] * 100,
+#         '性别': ['男', '女', '男'] * 100,
+#         '身高': ['男', '女', '男'] * 100,
+#         '体重': ['男', '女', '男'] * 100}
+# df = pd.DataFrame(data)
 
 # 创建 GridOptionsBuilder 对象
-gb = GridOptionsBuilder.from_dataframe(df)
+# gb = GridOptionsBuilder.from_dataframe(df)
 
 # 设置 AgGrid 组件的属性
-gridOptions = gb.build()
-gridOptions['onGridReady'] = "function(params) {params.api.setDomLayout('normal');params.api.sizeColumnsToFit();}"
-gridOptions['defaultColDef'] = {'flex': 1}
+# gridOptions = gb.build()
+# gridOptions['onGridReady'] = "function(params) {params.api.setDomLayout('normal');params.api.sizeColumnsToFit();}"
+# gridOptions['defaultColDef'] = {'flex': 1}
 
 # 使用 AgGrid 组件展示数据
 # grid_response = AgGrid(df, gridOptions=gridOptions, height=600, width='100%')
