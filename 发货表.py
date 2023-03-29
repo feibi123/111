910fae6b11c4d099362b9c5435a35728187578b0
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 13:15:42 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -3,18 +3,18 @@ import pandas as pd
 from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
 
 df = pd.DataFrame({
-    'A': [1,2,3,4,5,6],
-    'B': [11,22,33,44,55,66],
-    'C': [11,22,33,44,55,66],
-    'D': [11,22,33,44,55,66],
-    'E': [11,22,33,44,55,66],
+    'A': [1,2,3,4,5,6]*100,
+    'B': [11,22,33,44,55,66]*100,
+    'C': [11,22,33,44,55,66]*100,
+    'D': [11,22,33,44,55,66]*100,
+    'E': [11,22,33,44,55,66]*100,
 })
 
 # 创建GridOptionsBuilder对象
 gob = GridOptionsBuilder.from_dataframe(df)
 
 # 设置表格首行冻结
-gob.configure_grid_options(domLayout='normal', enableRangeSelection=True, enableCharts=True, enableStatusBar=True, enableFilter=True, enableSorting=True, enableColResize=True, enableCellChangeFlash=True, suppressDragLeaveHidesColumns=True, rowSelection='single', rowMultiSelectWithClick=True, suppressRowClickSelection=False, groupSelectsChildren=True, groupSelectsFiltered=True, suppressAggFuncInHeader=True, suppressMultiSort=False, rowHeight=22, headerHeight=28, defaultColDef={'sortable': True})
+gob.configure_grid_options(domLayout='normal', enableRangeSelection=True, enableCharts=True, enableStatusBar=True, enableFilter=True, enableSorting=True, enableColResize=True, enableCellChangeFlash=True, suppressDragLeaveHidesColumns=True, rowSelection='single', rowMultiSelectWithClick=True, suppressRowClickSelection=False, groupSelectsChildren=True, groupSelectsFiltered=True, suppressAggFuncInHeader=True, suppressMultiSort=False, rowHeight=22, headerHeight=28, defaultColDef={'sortable': True}, autoSizeColumns=True)
 
 # 获取GridOptions对象
 go = gob.build()
