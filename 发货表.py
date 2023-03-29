d321fdb5070583a69b2f718985a0d48ee0abea12
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 10:18:09 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -7,15 +7,16 @@
 
 import streamlit as st
 
-# 冻结上传文件按钮和大标题
-st.set_page_config(page_title="My Page", page_icon=":sunglasses:", layout="wide")
+# 冻结大标题和上传文件组件
+st.set_page_config(page_title="My Page", page_icon=":sunglasses:", layout="wide", initial_sidebar_state="collapsed")
 
 # 设置大标题
 st.title("My Title")
 
-# 创建一些示例文本
-lorem_ipsum = """Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus id dictum quam. Suspendisse mollis sagittis enim sit amet molestie. Vivamus ullamcorper sapien vitae risus rutrum ultrices. Proin bibendum, eros non dignissim consectetur, nisi tellus auctor tellus, et pharetra turpis enim vel dui. Nam id libero mi. Nullam porta congue euismod. Sed vel eleifend nunc. Donec iaculis ullamcorper nunc id pulvinar. Sed tristique efficitur lorem, id volutpat lacus interdum nec. Proin at turpis nunc. Proin imperdiet consequat eros non tristique. Ut elementum, turpis a lacinia convallis, odio enim pretium lorem, sit amet laoreet elit tellus vel elit. Donec in varius sapien, vel aliquam nisl. """
+# 添加上传文件组件
+uploaded_file = st.file_uploader("Choose a file")
 
 # 创建一个滚动区域来显示文本
 with st.beta_container():
+    lorem_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et ligula euismod, consequat dolor vel, rhoncus nibh. In semper gravida massa, ac eleifend mi ultrices a. Integer vitae diam sed mauris auctor fermentum. Fusce euismod neque vel turpis consectetur, in consectetur nisi rhoncus. Vestibulum vestibulum semper odio, a lacinia ex accumsan et. In hac habitasse platea dictumst. Sed tristique ex vel imperdiet blandit. Quisque posuere lectus lacus, eget iaculis ante feugiat in. Aliquam placerat quam id metus euismod, non ultrices nunc iaculis. Aliquam luctus nisi quam, ut elementum enim sagittis at. "
     st.markdown(f"<div style='height: 500px; overflow-y: scroll;'>{lorem_ipsum}</div>", unsafe_allow_html=True)
