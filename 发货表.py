28e7b354d38139e6af79f8433045a40494d9c861
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 10:58:59 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -9,6 +9,18 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
+st.markdown("""
+<style>
+    .fullScreenFrameWrapper {
+        width: 100% !important;
+    }
+    .element-container {
+        width: 100% !important;
+    }
+</style>
+""", unsafe_allow_html=True)
+
+# 在Streamlit中显示表格，并将表格全屏并冻结首行
 st.write(df.style.set_table_styles(
     [{"selector": "thead", "props": [("position", "sticky"), ("top", "0px")]}]
-).set_properties(**{'width': '100%', 'max-height': '500px', 'overflow-y': 'auto'}))
+).set_properties(**{'max-height': '500px', 'overflow-y': 'auto'}))
