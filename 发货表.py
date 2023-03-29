1593f273fbbb20955ce03456ee99af9880e68d89
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 14:03:10 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from st_aggrid import AgGrid, GridOptionsBuilder
 import pandas as pd
-
+st.set_page_config(page_title="AgGrid Example", layout="wide")
 # 创建示例数据
 data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '年龄': [18, 19, 20] * 100,
@@ -9,9 +9,6 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '身高': ['男', '女', '男'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
-
-# 设置页面宽度和高度
-st.set_page_config(page_title="AgGrid Example", layout="wide")
 
 # 创建 GridOptionsBuilder 对象
 gb = GridOptionsBuilder.from_dataframe(df)
