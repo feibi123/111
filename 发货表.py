e3d3ad348141ab6d6cdd2cdeb35e1ab96d6a1c4c
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 12:57:13 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -9,19 +9,27 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 设置表头固定和滚动
-st.markdown("""
-    <style>
-    .freeze-table {
-      overflow: auto;
-      max-height: 600px;
-    }
-    .freeze-table thead {
-      position: sticky;
-      top: 0;
-      background-color: white;
-    }
-    </style>
-""", unsafe_allow_html=True)
+# 设置表格样式
+table_style = """
+<style>
+table {
+  table-layout: fixed;
+}
+thead {
+  position: sticky;
+  top: 0;
+  background-color: white;
+}
+tbody {
+  overflow-y: scroll;
+  height: 400px;
+}
+th, td {
+  text-align: center;
+}
+</style>
+"""
 
-st.table(df.style.set_properties(**{'width': '100%', 'max-height': '500px', 'overflow-y': 'auto'}).set_table_attributes("class='freeze-table'"))
+# 显示表格
+st.markdown(table_style, unsafe_allow_html=True)
+st.markdown(df.to_html(index=False), unsafe_allow_html=True)
