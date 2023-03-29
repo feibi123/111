9748d9f8517253b082c112c73e67f45b98f45612
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-29 12:48:41 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -1,5 +1,5 @@
-import streamlit as st
 import pandas as pd
+import streamlit as st
 
 # 创建示例数据
 data = {'姓名': ['小明', '小红', '小刚'] * 100,
@@ -9,20 +9,11 @@ data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
-# 使用 CSS 控制表格样式
-st.write("""
-    <style>
-        div[data-testid="stTable"] thead {
-            position: sticky;
-            top: 0;
-            background-color: white;
-        }
-        div[data-testid="stTable"] tbody {
-            overflow-y: scroll;
-            max-height: 400px;
-        }
-    </style>
-""", unsafe_allow_html=True)
+# 设置表头固定
+styles = [dict(selector="thead", props=[("position", "sticky"), ("top", "0px")])]
 
-# 显示表格
-st.table(df)
+# 使用 Styler 将 DataFrame 转化为 HTML 格式
+html = df.style.set_table_styles(styles).render()
+
+# 渲染 HTML 字符串
+st.markdown(html, unsafe_allow_html=True)
