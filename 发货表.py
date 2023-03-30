23788d3fcaeb29ab61a9e16bdd4c7ad9dee07096
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-30 17:58:57 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -3,11 +3,11 @@ from st_aggrid import AgGrid, GridOptionsBuilder
 import pandas as pd
 
 # 创建示例数据
-data = {'姓名': ['小明', '小红', '小刚'] * 100,
-        '年龄': [18, 19, 20] * 100,
-        '性别': ['男nananananannanananan', '女', '男'] * 100,
-        '身高': ['男', '女', '男'] * 100,
-        '体重': ['男', '女', '男'] * 100}
+data = {'姓名nanananananananannananan': ['小明nanananananannana', '小红', '小刚'] * 100,
+        '年nananananananananan龄': [18, 19, 20] * 100,
+        '性nananananan别': ['男nananananannanananan', '女', '男'] * 100,
+        '身nanananan高': ['男', '女', '男'] * 100,
+        '体ananananannananananan重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
 
 gb = GridOptionsBuilder.from_dataframe(df)
