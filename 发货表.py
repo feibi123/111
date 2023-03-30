eba567cfe6f07a1792ba9874da9c1c0e7e389e22
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-30 17:58:13 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -5,7 +5,7 @@ import pandas as pd
 # 创建示例数据
 data = {'姓名': ['小明', '小红', '小刚'] * 100,
         '年龄': [18, 19, 20] * 100,
-        '性别': ['男', '女', '男'] * 100,
+        '性别': ['男nananananannanananan', '女', '男'] * 100,
         '身高': ['男', '女', '男'] * 100,
         '体重': ['男', '女', '男'] * 100}
 df = pd.DataFrame(data)
@@ -17,8 +17,9 @@ st.set_page_config(page_title="AgGrid Example", layout="wide")
 
 # 设置 AgGrid 组件的属性
 gridOptions = gb.build()
-gridOptions['onGridReady'] = "function(params) {params.api.setDomLayout('normal');params.api.sizeColumnsToFit();}"
+gridOptions['domLayout'] = 'normal'
 gridOptions['defaultColDef'] = {'flex': 1}
+gridOptions['onFirstDataRendered'] = 'function(params) {params.api.sizeColumnsToFit();}'
 
 window_height = st.experimental_get_query_params().get('height', [None])[0]
 
