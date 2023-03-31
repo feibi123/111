403223a283fb420f9d76a5998a9ebd331680f54f
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-03-31 10:51:41 +0800

    Update 发货表.py

--- 发货表.py
+++ 发货表.py
@@ -27,15 +27,15 @@ gridOptions['columnDefs'] = [
 
 gridOptions['defaultColDef'] = {'resizable': True}
 
-# window_height = st.experimental_get_query_params().get('height', [None])[0]
+window_height = st.experimental_get_query_params().get('height', [None])[0]
 
-# if window_height:
-#     window_height = int(window_height.replace('px', ''))
-# else:
-#     window_height = None  # 设置一个默认值
+if window_height:
+    window_height = int(window_height.replace('px', ''))
+else:
+    window_height = None  # 设置一个默认值
 
 
-# window_width = '100%'
-# # 使用 AgGrid 组件展示数据
-# grid_response = AgGrid(df, gridOptions=gridOptions, height=window_height, width='100%')
-grid_response = AgGrid(df, gridOptions=gridOptions, height=600, width='100%')          
+window_width = '100%'
+# 使用 AgGrid 组件展示数据
+grid_response = AgGrid(df, gridOptions=gridOptions, height=window_height, width='100%')
+# grid_response = AgGrid(df, gridOptions=gridOptions, height=600, width='100%')          
