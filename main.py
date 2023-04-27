3e18942ac804764aa8ea77bbc48893957bb919a0
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-04-27 14:28:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ import streamlit as st
 import pandas as pd
 import requests
 from io import StringIO
+from urllib.parse import unquote
 
 # 获取文件夹中所有csv文件的URL列表
 url_folder = "https://api.github.com/repos/feibi123/111/contents/广告?ref=main"
@@ -26,14 +27,14 @@ data = response.json()
 csv_urls = [x["download_url"] for x in data if x["name"].endswith(".csv")]
 
 # 读取所有CSV文件并合并为一个DataFrame
-df_list = []
+dfs = []
 for url in csv_urls:
     content = requests.get(url).content.decode("utf-8")
     sheet_name = url.split("/")[-1].split(".")[0]
     df = pd.read_csv(StringIO(content))
-    df["sheet_name"] = pd.to_datetime(sheet_name, format='%Y%m%d')
-    df_list.append(df)
-df = pd.concat(df_list)
+    df["sheet_name"] = pd.to_datetime(unquote(sheet_name), format='%Y%m%d')
+    dfs.append(df)
+df = pd.concat(dfs)
 
 # 在Streamlit中显示数据
 st.dataframe(df)
