8f768bae7d0ae23ce5588e4702e0f08a5941ff9e
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-04-27 15:17:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,5 +31,64 @@ df['访问量总计'] = df['手机端访问量'] + df['PC端访问量']
 df['总订单'] = df['已订购商品数量'] + df['已订购商品数量 - B2B']
 df['日期'] = pd.to_datetime(df['日期'])
 df['日期'] = df['日期'].dt.date
-# 在Streamlit中显示数据
-st.dataframe(df)
+uploaded_file = st.sidebar.file_uploader("上传产品属性表", type="xlsx")
+product_df = pd.read_excel(uploaded_file)
+df = pd.merge(df, product_df, on='SKU', how='left')
+columns1 = ['日期', '链接名称', '父ASIN', 'SKU', '手机端访问量', 'PC端访问量', '访问量总计', '总订单']
+df = df[columns1]
+
+uploaded_file1 = st.sidebar.file_uploader("上传产品属性表", type="xlsx")
+dt = pd.read_excel(uploaded_file1)
+
+dt['日期'] = pd.to_datetime(dt['日期']).dt.strftime('%Y-%m-%d')
+dt = dt.rename(columns={'广告SKU': 'SKU'})
+dz = dt[dt["广告活动名称"].str.contains("自动")]  # 自动广告汇总
+pivot1 = pd.pivot_table(
+    dz,
+    values=['点击量', '7天总销售量(#)'],
+    index=['日期', 'SKU'],
+    columns=None,
+    aggfunc='sum',
+    fill_value=0,
+    dropna=True,
+    margins=False,
+)
+pivot1.reset_index(inplace=True)                # 自动广告点击量
+pivot1 = pivot1.rename(columns={'点击量': '自动广告点击量'})
+pivot1 = pivot1.rename(columns={'7天总销售量(#)': '自动广告销量'})
+pivot1 = pd.merge(pivot1, product_df, on='SKU', how='left')
+
+
+ds = dt[~dt["广告活动名称"].str.contains("自动")]   # 手动广告汇总
+pivot2 = pd.pivot_table(
+    ds,
+    values=['点击量', '7天总销售量(#)'],
+    index=['日期', 'SKU'],
+    columns=None,
+    aggfunc='sum',
+    fill_value=0,
+    dropna=True,
+    margins=False,
+)
+pivot2.reset_index(inplace=True)                # 自动广告点击量
+pivot2 = pivot2.rename(columns={'点击量': '手动广告点击量'})
+pivot2 = pivot2.rename(columns={'7天总销售量(#)': '手动广告销量'})
+pivot2 = pd.merge(pivot2, product_df, on='SKU', how='left')
+
+pivot = pd.concat([df, pivot1, pivot2], axis=0, ignore_index=True)
+pivot = pivot.groupby(['日期', 'SKU', '链接名称', '父ASIN'], as_index=False).sum()
+pivot['广告单'] = pivot['手动广告销量'] + pivot['自动广告销量']
+pivot['正常单'] = pivot['总订单'] - pivot['广告单']
+pivot['正常单'] = np.clip(pivot['正常单'], 0, np.inf)
+pivot['正常单占比'] = pivot.apply(lambda x: '{:.2%}'.format(x['正常单'] / x['总订单']) if x['总订单'] != 0 else '0.00%', axis=1)
+pivot['综合转化率'] = pivot.apply(
+    lambda x: '{:.2%}'.format(x['总订单'] / (x['访问量总计'] + x['自动广告点击量'] + x['手动广告点击量']))
+    if x['访问量总计'] + x['自动广告点击量'] + x['手动广告点击量'] != 0 else '0.00%', axis=1)
+keep_columns = ['日期', '链接名称', '父ASIN', 'SKU', '手机端访问量', 'PC端访问量', '访问量总计', '自动广告点击量', '手动广告点击量', '广告单', '正常单',
+                '总订单', '正常单占比', '综合转化率']
+dt = pivot[keep_columns].astype({'手机端访问量': int, 'PC端访问量': int, '访问量总计': int, '自动广告点击量': int, '手动广告点击量': int,
+                                 '广告单': int, '正常单': int, '总订单': int})
+
+
+
+st.table(df)
