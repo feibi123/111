9955fdc2f87909e022e2377528d3d425c5834742
feibi123 <126564708+feibi123@users.noreply.github.com>
2023-04-27 14:11:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import streamlit as st
 import pandas as pd
 
 # 从GitHub加载数据
-url = "https://raw.githubusercontent.com/feibi123/111.git/main/广告/2023年3月10日.csv"
+url = "https://raw.githubusercontent.com/feibi123/111/main/广告/2023年3月10日.csv"
 data = pd.read_csv(url)
 
 # 在Streamlit中显示数据
-st.dataframe(data)
+st.write(data)
